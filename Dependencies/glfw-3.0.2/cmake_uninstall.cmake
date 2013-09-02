300fa6bd10e37337a56d18730243f64fadc7e4a7
knight666 <knight666@gmail.com>
2013-09-02 15:00:54 +0200

    Dependencies can be built using the ExLibrisGL.Dependencies solution.

---
+++ Dependencies/glfw-3.0.2/cmake_uninstall.cmake
@@ -0,0 +1,29 @@
+
+if (NOT EXISTS "H:/Projects/ExLibrisGL/Dependencies/glfw-3.0.2/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: \"H:/Projects/ExLibrisGL/Dependencies/glfw-3.0.2/install_manifest.txt\"")
+endif()
+
+file(READ "H:/Projects/ExLibrisGL/Dependencies/glfw-3.0.2/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+
+foreach (file ${files})
+  message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+  if (EXISTS "$ENV{DESTDIR}${file}")
+    exec_program("E:/Programs/CMake 2.8/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif()
+  elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
+    EXEC_PROGRAM("E:/Programs/CMake 2.8/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing symlink \"$ENV{DESTDIR}${file}\"")
+    endif()
+  else()
+    message(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
+  endif()
+endforeach()
+
