725aa5dfa140c0de65bd0d42e2ecbe0e88718ede
knight666 <knight666@gmail.com>
2013-09-02 21:42:57 +0200

    Main: Glyphs are rendered to bitmaps and used to render to a texture.

--- Projects/Example/Main.cpp
+++ Projects/Example/Main.cpp
@@ -1,7 +1,10 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string>
 #include <map>
+#include <vector>
 
 #include "FaceMetrics.h"
 #include "FontLoaderFreetype.h"
@@ -18,6 +21,99 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
+struct GlyphBitmap
+{
+	unsigned char* pixels;
+	unsigned int width;
+	unsigned int height;
+};
+
+struct Glyph
+{
+	ExLibris::GlyphMetrics* metrics;
+	GlyphBitmap* bitmap;
+};
+
+struct GlyphVertex
+{
+	enum Offset
+	{
+		eOffset_Position = 0,
+		eOffset_TextureCoordinate = sizeof(glm::vec2)
+	};
+
+	glm::vec2 position;
+	glm::vec2 texture_coordinate;
+};
+
+GLuint CreateTexture(const std::map<unsigned int, Glyph*>& a_Glyphs, unsigned int a_LineHeight, const std::wstring& a_Text)
+{
+	unsigned int texture_width = 0;
+	unsigned int texture_height = a_LineHeight;
+	std::vector<Glyph*> glyphs;
+
+	for (std::wstring::const_iterator char_it = a_Text.begin(); char_it != a_Text.end(); ++char_it)
+	{
+		unsigned int codepoint = (unsigned int)*char_it;
+
+		std::map<unsigned int, Glyph*>::const_iterator glyph_found = a_Glyphs.find(codepoint);
+		if (glyph_found != a_Glyphs.end())
+		{
+			Glyph* glyph = glyph_found->second;
+
+			texture_width += (unsigned int)glyph->metrics->advance;
+
+			glyphs.push_back(glyph);
+		}
+	}
+
+	unsigned char* texture_data = new unsigned char[texture_width * texture_height * 4];
+	memset(texture_data, 0, texture_width * texture_height * 4);
+
+	unsigned int dst_pitch = texture_width * 4;
+	unsigned int cursor_x = 0;
+
+	for (std::vector<Glyph*>::iterator glyph_it = glyphs.begin(); glyph_it != glyphs.end(); ++glyph_it)
+	{
+		Glyph* glyph = *glyph_it;
+
+		ExLibris::GlyphMetrics* metrics = glyph->metrics;
+		GlyphBitmap* bitmap = glyph->bitmap;
+
+		unsigned char* dst = texture_data + (unsigned int)(dst_pitch * metrics->offset.y) + (unsigned int)(metrics->offset.x * 4) + (cursor_x * 4);
+
+		unsigned char* src = bitmap->pixels;
+		unsigned int src_pitch = bitmap->width * 4;
+
+		for (unsigned int y = 0; y < bitmap->height; ++y)
+		{
+			memcpy(dst, src, src_pitch);
+
+			dst += dst_pitch;
+			src += src_pitch;
+		}
+
+		cursor_x += (unsigned int)metrics->advance;
+	}
+
+	GLuint texture = 0;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(
+		GL_TEXTURE_2D, 0, GL_RGBA,
+		texture_width, texture_height,
+		0,
+		GL_BGRA, GL_UNSIGNED_BYTE, texture_data
+	);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	return texture;
+}
+
 int main(int argc, const char** argv)
 {
 	ExLibris::FontLoaderFreetype loader;
@@ -26,13 +122,17 @@ int main(int argc, const char** argv)
 	FT_Error errors = 0;
 
 	errors = FT_Set_Char_Size(font_face, 0, 24 << 6, 0, 96);
+	float font_face_height = (float)(font_face->size->metrics.height >> 6);
 
 	ExLibris::FaceMetrics face_metrics;
+	std::map<unsigned int, Glyph*> font_glyphs;
 
 	FT_UInt codepoint = 0;
 	FT_ULong glyph_index = FT_Get_First_Char(font_face, &codepoint);
 	do
 	{
+		Glyph* cached = new Glyph;
+
 		errors = FT_Load_Glyph(font_face, codepoint, FT_LOAD_DEFAULT);
 		FT_Glyph_Metrics& glyph_metrics = font_face->glyph->metrics;
 
@@ -44,6 +144,45 @@ int main(int argc, const char** argv)
 
 		face_metrics.AddGlyphMetrics(metrics);
 
+		cached->metrics = metrics;
+
+		errors = FT_Render_Glyph(font_face->glyph, FT_RENDER_MODE_NORMAL);
+		FT_Bitmap& glyph_bitmap = font_face->glyph->bitmap;
+
+		cached->bitmap = new GlyphBitmap;
+		cached->bitmap->width = glyph_bitmap.width;
+		cached->bitmap->height = glyph_bitmap.rows;
+		cached->bitmap->pixels = new unsigned char[cached->bitmap->width * cached->bitmap->height * 4];
+
+		unsigned char* src_line = glyph_bitmap.buffer;
+		unsigned int src_pitch = glyph_bitmap.pitch;
+
+		unsigned char* dst_line = cached->bitmap->pixels;
+		unsigned int dst_pitch = cached->bitmap->width * 4;
+
+		for (int y = 0; y < glyph_bitmap.rows; y++)
+		{
+			unsigned char* src = src_line;
+			unsigned char* dst = dst_line;
+
+			for (int x = 0; x < glyph_bitmap.width; x++)
+			{
+				char value = *src;
+				dst[0] = value;
+				dst[1] = value;
+				dst[2] = value;
+				dst[3] = value;
+
+				dst += 4;
+				src++;
+			}
+
+			src_line += src_pitch;
+			dst_line += dst_pitch;
+		}
+
+		font_glyphs.insert(std::make_pair(cached->metrics->codepoint, cached));
+
 		glyph_index = FT_Get_Next_Char(font_face, glyph_index, &codepoint);
 	}
 	while (codepoint != 0);
@@ -52,13 +191,13 @@ int main(int argc, const char** argv)
 
 	glfwSetErrorCallback(OnGlfwError);
 
-	if (!glfwInit())
+	if (glfwInit() == 0)
 	{
 		exit(EXIT_FAILURE);
 	}
 
-	window = glfwCreateWindow(640, 480, "Simple example", nullptr, nullptr);
-	if (!window)
+	window = glfwCreateWindow(640, 480, "ExLibris", nullptr, nullptr);
+	if (window == nullptr)
 	{
 		glfwTerminate();
 		exit(EXIT_FAILURE);
@@ -66,7 +205,68 @@ int main(int argc, const char** argv)
 
 	glfwMakeContextCurrent(window);
 
-	while (!glfwWindowShouldClose(window))
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW." << std::endl;
+		return false;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_TEXTURE_2D);
+
+	std::wstring text = L"Hello World!";
+	GLuint text_texture = CreateTexture(font_glyphs, (unsigned int)font_face_height, text);
+
+	GLuint vertex_buffer;
+	glGenBuffers(1, &vertex_buffer);
+	unsigned int vertex_total = 4;
+	GlyphVertex* vertex_data = new GlyphVertex[vertex_total];
+
+	GLuint element_buffer;
+	glGenBuffers(1, &element_buffer);
+	unsigned int element_total = 6;
+	GLuint* element_data = new GLuint[element_total];
+
+	vertex_data[0].position.x = 0.0f;
+	vertex_data[0].position.y = 0.0f;
+	vertex_data[0].texture_coordinate.x = 0.0f;
+	vertex_data[0].texture_coordinate.y = 1.0f;
+
+	vertex_data[1].position.x = 2.0f;
+	vertex_data[1].position.y = 0.0f;
+	vertex_data[1].texture_coordinate.x = 1.0f;
+	vertex_data[1].texture_coordinate.y = 1.0f;
+
+	vertex_data[2].position.x = 0.0f;
+	vertex_data[2].position.y = 1.0f;
+	vertex_data[2].texture_coordinate.x = 0.0f;
+	vertex_data[2].texture_coordinate.y = 0.0f;
+
+	vertex_data[3].position.x = 2.0f;
+	vertex_data[3].position.y = 1.0f;
+	vertex_data[3].texture_coordinate.x = 1.0f;
+	vertex_data[3].texture_coordinate.y = 0.0f;
+
+	element_data[0] = 1;
+	element_data[1] = 0;
+	element_data[2] = 2;
+
+	element_data[3] = 1;
+	element_data[4] = 2;
+	element_data[5] = 3;
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, vertex_total * sizeof(GlyphVertex), vertex_data, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, element_total * sizeof(GLuint), element_data, GL_STATIC_DRAW);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	while (glfwWindowShouldClose(window) == 0)
 	{
 		float ratio;
 		int width, height;
@@ -75,24 +275,33 @@ int main(int argc, const char** argv)
 		ratio = width / (float) height;
 
 		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
 		glMatrixMode(GL_MODELVIEW);
 
-		glLoadIdentity();
-		glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, text_texture);
 
-		glBegin(GL_TRIANGLES);
-		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
-		glVertex3f(0.f, 0.6f, 0.f);
-		glEnd();
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer);
+
+		glVertexPointer(2, GL_FLOAT, sizeof(GlyphVertex), (void*)GlyphVertex::eOffset_Position);
+		glTexCoordPointer(2, GL_FLOAT, sizeof(GlyphVertex), (void*)GlyphVertex::eOffset_TextureCoordinate);
+
+		glDrawElements(GL_TRIANGLES, element_total, GL_UNSIGNED_INT, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+		glDisable(GL_BLEND);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
