5a2904295ecc3073fd897984504e6b0630997a62
Anne <knight666+github@gmail.com>
2013-09-06 14:43:55 +0200

    Suite.FontFace: Added tests that use mocked font instance. Mock.Font: Mocked out font implementation.

---
+++ Projects/ExLibris.Test/Mock.Font.h
@@ -0,0 +1,43 @@
+#ifndef _MOCK_FONT_H_
+#define _MOCK_FONT_H_
+
+#include <IFont.h>
+#include <FontFace.h>
+
+namespace ExLibris
+{
+
+	class MockFont
+		: public IFont
+	{
+	
+	public:
+	
+		MockFont(const std::string& a_FamilyName)
+			: IFont(a_FamilyName)
+		{
+		}
+
+		~MockFont()
+		{
+		}
+
+		unsigned int GetIndexFromCodepoint(unsigned int a_CodepointUtf32) const
+		{
+			return a_CodepointUtf32;
+		}
+
+		FontFace* CreateFace(float a_Size) const
+		{
+			FontFace* face = new FontFace(this);
+			face->SetSize(a_Size);
+			face->SetLineHeight(a_Size * 2.0f);
+
+			return face;
+		}
+	
+	}; // class MockFont
+
+}; // namespace ExLibris
+
+#endif
