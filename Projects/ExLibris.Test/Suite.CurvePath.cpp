d4690e418779bb231c9f926030e76fe3b835f539
Anne <knight666+github@gmail.com>
2013-09-12 08:09:17 +0200

    Suite.CurvePath: Added tests for positioning pen and two lines. CurveSettings: Settings used for converting curve to points. ICurvePathVisitor: Renamed VisitShape* methods to VisitCurve*.

--- Projects/ExLibris.Test/Suite.CurvePath.cpp
+++ Projects/ExLibris.Test/Suite.CurvePath.cpp
@@ -14,19 +14,19 @@ public:
 	{
 	}
 
-	void VisitShapeStart()
+	void VisitCurveStart()
 	{
 		Contour contour;
 		contours.push_back(contour);
 	}
 
-	void VisitShapePosition(const glm::vec2& a_Position)
+	void VisitCurvePosition(const glm::vec2& a_Position)
 	{
 		Contour& contour = contours.back();
 		contour.positions.push_back(a_Position);
 	}
 
-	void VisitShapeEnd()
+	void VisitCurveEnd()
 	{
 	}
 
@@ -40,14 +40,32 @@ public:
 
 };
 
+TEST(CurvePath, PathMove)
+{
+	CurvePath path;
+	path.Move(glm::vec2(33.3f, 15.5f));
+
+	CurveSettings settings;
+
+	MockCurvePathVisitor visitor;
+	path.Accept(visitor, settings);
+
+	ASSERT_EQ(1, visitor.contours.size());
+	ASSERT_EQ(1, visitor.contours[0].positions.size());
+	EXPECT_FLOAT_EQ(33.3f, visitor.contours[0].positions[0].x);
+	EXPECT_FLOAT_EQ(15.5f, visitor.contours[0].positions[0].y);
+}
+
 TEST(CurvePath, PathLine)
 {
 	CurvePath path;
 	path.Move(glm::vec2(10.0f, 10.0f));
 	path.LineTo(glm::vec2(25.0f, 10.0f));
 
+	CurveSettings settings;
+
 	MockCurvePathVisitor visitor;
-	path.Accept(visitor);
+	path.Accept(visitor, settings);
 
 	ASSERT_EQ(1, visitor.contours.size());
 	ASSERT_EQ(2, visitor.contours[0].positions.size());
@@ -55,4 +73,26 @@ TEST(CurvePath, PathLine)
 	EXPECT_FLOAT_EQ(10.0f, visitor.contours[0].positions[0].y);
 	EXPECT_FLOAT_EQ(25.0f, visitor.contours[0].positions[1].x);
 	EXPECT_FLOAT_EQ(10.0f, visitor.contours[0].positions[1].y);
+}
+
+TEST(CurvePath, PathTwoLines)
+{
+	CurvePath path;
+	path.Move(glm::vec2(6.0f, 16.0f));
+	path.LineTo(glm::vec2(2.0f, 3.0f));
+	path.LineTo(glm::vec2(17.0f, 17.0f));
+
+	CurveSettings settings;
+
+	MockCurvePathVisitor visitor;
+	path.Accept(visitor, settings);
+
+	ASSERT_EQ(1, visitor.contours.size());
+	ASSERT_EQ(3, visitor.contours[0].positions.size());
+	EXPECT_FLOAT_EQ(6.0f, visitor.contours[0].positions[0].x);
+	EXPECT_FLOAT_EQ(16.0f, visitor.contours[0].positions[0].y);
+	EXPECT_FLOAT_EQ(2.0f, visitor.contours[0].positions[1].x);
+	EXPECT_FLOAT_EQ(3.0f, visitor.contours[0].positions[1].y);
+	EXPECT_FLOAT_EQ(17.0f, visitor.contours[0].positions[2].x);
+	EXPECT_FLOAT_EQ(17.0f, visitor.contours[0].positions[2].y);
 }
