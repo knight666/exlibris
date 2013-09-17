d50bc8c23893f3d5cbfc13d27a45e15c8006eaea
Anne <knight666+github@gmail.com>
2013-09-17 17:41:40 +0200

    Polygon: Triangulate uses libpoly2tri to triangulate data. Polygon: Triangulate returns a pointer. TriangleList: Vertices are stored as a raw array.

--- Projects/ExLibris.Test/Suite.Polygon.cpp
+++ Projects/ExLibris.Test/Suite.Polygon.cpp
@@ -8,9 +8,10 @@ TEST(Polygon, TriangulateEmpty)
 {
 	Polygon polygon;
 
-	TriangleList list = polygon.Triangulate();
+	TriangleList* list = polygon.Triangulate();
 
-	ASSERT_EQ(0, list.positions.size());
+	EXPECT_EQ(nullptr, list->positions);
+	EXPECT_EQ(0, list->vertex_count);
 }
 
 TEST(Polygon, TriangulateOne)
@@ -24,15 +25,16 @@ TEST(Polygon, TriangulateOne)
 	
 	polygon.AddShape(shape);
 
-	TriangleList list = polygon.Triangulate();
+	TriangleList* list = polygon.Triangulate();
 
-	ASSERT_EQ(3, list.positions.size());
-	EXPECT_FLOAT_EQ(56.9f, list.positions[0].x);
-	EXPECT_FLOAT_EQ(-91.9f, list.positions[0].y);
-	EXPECT_FLOAT_EQ(6.43f, list.positions[1].x);
-	EXPECT_FLOAT_EQ(12.8f, list.positions[1].y);
-	EXPECT_FLOAT_EQ(-11.9f, list.positions[2].x);
-	EXPECT_FLOAT_EQ(14.5f, list.positions[2].y);
+	ASSERT_NE(nullptr, list->positions);
+	ASSERT_EQ(3, list->vertex_count);
+	EXPECT_FLOAT_EQ(6.43f, list->positions[0].x);
+	EXPECT_FLOAT_EQ(12.8f, list->positions[0].y);
+	EXPECT_FLOAT_EQ(-11.9f, list->positions[1].x);
+	EXPECT_FLOAT_EQ(14.5f, list->positions[1].y);
+	EXPECT_FLOAT_EQ(56.9f, list->positions[2].x);
+	EXPECT_FLOAT_EQ(-91.9f, list->positions[2].y);
 }
 
 TEST(Polygon, TriangulateTwo)
@@ -47,19 +49,21 @@ TEST(Polygon, TriangulateTwo)
 
 	polygon.AddShape(shape);
 
-	TriangleList list = polygon.Triangulate();
+	TriangleList* list = polygon.Triangulate();
 
-	ASSERT_EQ(6, list.positions.size());
-	EXPECT_FLOAT_EQ(16.4f, list.positions[0].x);
-	EXPECT_FLOAT_EQ(78.8f, list.positions[0].y);
-	EXPECT_FLOAT_EQ(12.9f, list.positions[1].x);
-	EXPECT_FLOAT_EQ(105.9f, list.positions[1].y);
-	EXPECT_FLOAT_EQ(-25.9f, list.positions[2].x);
-	EXPECT_FLOAT_EQ(56.0f, list.positions[2].y);
-	EXPECT_FLOAT_EQ(12.9f, list.positions[3].x);
-	EXPECT_FLOAT_EQ(105.9f, list.positions[3].y);
-	EXPECT_FLOAT_EQ(-25.9f, list.positions[4].x);
-	EXPECT_FLOAT_EQ(56.0f, list.positions[4].y);
-	EXPECT_FLOAT_EQ(-125.0f, list.positions[5].x);
-	EXPECT_FLOAT_EQ(32.9f, list.positions[5].y);
+	ASSERT_NE(nullptr, list->positions);
+	ASSERT_EQ(12, list->vertex_count);
+	EXPECT_FLOAT_EQ(-25.9f, list->positions[0].x);
+	EXPECT_FLOAT_EQ(56.0f, list->positions[0].y);
+	EXPECT_FLOAT_EQ(-125.0f, list->positions[1].x);
+	EXPECT_FLOAT_EQ(32.9f, list->positions[1].y);
+	EXPECT_FLOAT_EQ(58.82f, list->positions[2].x);
+	EXPECT_FLOAT_EQ(11.0f, list->positions[2].y);
+	EXPECT_FLOAT_EQ(16.4f, list->positions[3].x);
+	EXPECT_FLOAT_EQ(78.8f, list->positions[3].y);
+	EXPECT_FLOAT_EQ(-25.9f, list->positions[4].x);
+	EXPECT_FLOAT_EQ(56.0f, list->positions[4].y);
+	EXPECT_FLOAT_EQ(58.82f, list->positions[5].x);
+	EXPECT_FLOAT_EQ(11.0f, list->positions[5].y);
+	
 }
