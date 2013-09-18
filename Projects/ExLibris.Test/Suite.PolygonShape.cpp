3ae6698f6769a5d1ff9870eed20f0262c9ffe727
Anne <knight666@gmail.com>
2013-09-18 17:41:57 +0200

    Suite.PolygonShape: Added tests for triangulating multiple polygons.

--- Projects/ExLibris.Test/Suite.PolygonShape.cpp
+++ Projects/ExLibris.Test/Suite.PolygonShape.cpp
@@ -65,5 +65,86 @@ TEST(PolygonShape, TriangulateTwo)
 	EXPECT_FLOAT_EQ(56.0f, list->positions[4].y);
 	EXPECT_FLOAT_EQ(58.82f, list->positions[5].x);
 	EXPECT_FLOAT_EQ(11.0f, list->positions[5].y);
+}
+
+TEST(PolygonShape, TriangulateTwoPolygons)
+{
+	PolygonShape shape;
+
+	Polygon polygon_first;
+	polygon_first.positions.push_back(glm::vec2(-20.0f, -10.0f));
+	polygon_first.positions.push_back(glm::vec2(30.0f, 10.0f));
+	polygon_first.positions.push_back(glm::vec2(30.0f, 55.0f));
+
+	Polygon polygon_second;
+	polygon_second.positions.push_back(glm::vec2(50.0f, -20.0f));
+	polygon_second.positions.push_back(glm::vec2(80.0f, 10.0f));
+	polygon_second.positions.push_back(glm::vec2(120.0f, 20.0f));
+
+	shape.AddShape(polygon_first);
+	shape.AddShape(polygon_second);
+
+	TriangleList* list = shape.Triangulate();
+
+	ASSERT_NE(nullptr, list->positions);
+	ASSERT_EQ(18, list->vertex_count);
+	EXPECT_FLOAT_EQ(-20.0f, list->positions[0].x);
+	EXPECT_FLOAT_EQ(-10.0f, list->positions[0].y);
+	EXPECT_FLOAT_EQ(30.0f, list->positions[1].x);
+	EXPECT_FLOAT_EQ(10.0f, list->positions[1].y);
+	EXPECT_FLOAT_EQ(30.0f, list->positions[2].x);
+	EXPECT_FLOAT_EQ(55.0f, list->positions[2].y);
+	EXPECT_FLOAT_EQ(50.0f, list->positions[3].x);
+	EXPECT_FLOAT_EQ(-20.0f, list->positions[3].y);
+	EXPECT_FLOAT_EQ(80.0f, list->positions[4].x);
+	EXPECT_FLOAT_EQ(10.0f, list->positions[4].y);
+	EXPECT_FLOAT_EQ(120.0f, list->positions[5].x);
+	EXPECT_FLOAT_EQ(20.0f, list->positions[5].y);
+}
+
+TEST(PolygonShape, TriangulateTwoPolygonsHole)
+{
+	// o ------------- o
+	// |               |
+	// |       o       |
+	// |     /   \     |
+	// |    /     \    |
+	// |   o ----- o   |
+	// |               |
+	// o --------------o
+
+	PolygonShape shape;
+
+	Polygon polygon_first;
+	polygon_first.positions.push_back(glm::vec2(-50.0f, -50.0f));
+	polygon_first.positions.push_back(glm::vec2(50.0f, -50.0f));
+	polygon_first.positions.push_back(glm::vec2(50.0f, 50.0f));
+	polygon_first.positions.push_back(glm::vec2(-50.0f, 50.0f));
+
+	Polygon polygon_second;
+	polygon_second.positions.push_back(glm::vec2(0.0f, -25.0f));
+	polygon_second.positions.push_back(glm::vec2(-25.0f, 25.0f));
+	polygon_second.positions.push_back(glm::vec2(25.0f, 25.0f));
+
+	shape.AddShape(polygon_first);
+	shape.AddShape(polygon_second);
+
+	TriangleList* list = shape.Triangulate();
+
+	ASSERT_NE(nullptr, list->positions);
+	ASSERT_EQ(21, list->vertex_count);
 	
+	EXPECT_FLOAT_EQ(-50.0f, list->positions[0].x);
+	EXPECT_FLOAT_EQ(50.0f, list->positions[0].y);
+	EXPECT_FLOAT_EQ(-25.0f, list->positions[1].x);
+	EXPECT_FLOAT_EQ(25.0f, list->positions[1].y);
+	EXPECT_FLOAT_EQ(50.0f, list->positions[2].x);
+	EXPECT_FLOAT_EQ(50.0f, list->positions[2].y);
+
+	EXPECT_FLOAT_EQ(-25.0f, list->positions[18].x);
+	EXPECT_FLOAT_EQ(25.0f, list->positions[18].y);
+	EXPECT_FLOAT_EQ(25.0f, list->positions[19].x);
+	EXPECT_FLOAT_EQ(25.0f, list->positions[19].y);
+	EXPECT_FLOAT_EQ(50.0f, list->positions[20].x);
+	EXPECT_FLOAT_EQ(50.0f, list->positions[20].y);
 }
