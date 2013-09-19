2196b9ed1a0a5a59a8d44173e12c0c0c9bd06854
Anne <knight666+github@gmail.com>
2013-09-19 08:26:10 +0200

    FontFreetype: Fixed generating meshes. Main: Fixed rendering of glyph meshes.

--- Projects/ExLibrisGL/GlyphOutline.h
+++ Projects/ExLibrisGL/GlyphOutline.h
@@ -1,25 +1,14 @@
 #ifndef _GLYPHOUTLINE_H_
 #define _GLYPHOUTLINE_H_
 
-#include "GlyphContour.h"
+#include "Polygon.h"
 
 namespace ExLibris
 {
 
 	struct GlyphOutline
 	{
-		~GlyphOutline()
-		{
-			for (std::vector<GlyphContour*>::iterator contour_it = contours.begin(); contour_it != contours.end(); ++contour_it)
-			{
-				delete *contour_it;
-			}
-			contours.clear();
-		}
-	
-	public:
-
-		std::vector<GlyphContour*> contours;
+		std::vector<Polygon> contours;
 	};
 
 }; // namespace ExLibris
