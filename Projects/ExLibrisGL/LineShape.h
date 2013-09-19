e95a7a42fee202461acc2eda847ad1e79ad250da
Anne <knight666@gmail.com>
2013-09-19 17:49:44 +0200

    LineShape: Class for converting polygons to line meshes. Suite.LineShape: Tests for line shape class.

---
+++ Projects/ExLibrisGL/LineShape.h
@@ -0,0 +1,33 @@
+#ifndef _LINESHAPE_H_
+#define _LINESHAPE_H_
+
+#include "Polygon.h"
+#include "TriangleList.h"
+
+namespace ExLibris
+{
+
+	class LineShape
+	{
+
+	public:
+	
+		LineShape();
+		~LineShape();
+
+		size_t GetPolygonCount() const;
+
+		void Clear();
+		void AddPolygon(const Polygon& a_Polygon);
+
+		TriangleList* Triangulate() const;
+
+	private:
+
+		std::vector<Polygon> m_Polygons;
+
+	}; // class LineShape
+
+}; // namespace ExLibris
+
+#endif
