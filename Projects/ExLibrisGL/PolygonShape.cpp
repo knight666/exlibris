c8ce7f302560d175ee2fa6d1bd5f04bfb8894934
Anne <knight666+github@gmail.com>
2013-09-19 08:00:55 +0200

    PolygonShape: Fixed issue where separate polygons would be merged into a single triangle list.

--- Projects/ExLibrisGL/PolygonShape.cpp
+++ Projects/ExLibrisGL/PolygonShape.cpp
@@ -38,6 +38,30 @@ namespace ExLibris
 		}
 	}
 
+	struct PolyConvert
+	{
+		PolyConvert()
+			: polygon(nullptr)
+			, cdt(nullptr)
+		{
+		}
+
+		~PolyConvert()
+		{
+			if (cdt != nullptr)
+			{
+				delete cdt;
+				cdt = nullptr;
+			}
+		}
+
+		const Polygon* polygon;
+		std::vector<p2t::Point*> polyline;
+		std::vector<const Polygon*> holes;
+		p2t::CDT* cdt;
+		std::vector<p2t::Triangle*> triangles;
+	};
+
 	// if an object is returned instead of a pointer, the destructor is called on return
 	// compiler bug?
 
@@ -50,70 +74,41 @@ namespace ExLibris
 			return triangles;
 		}
 
-		const Polygon& base_polygon = *m_Polygons.begin();
+		std::vector<PolyConvert*> converted;
+		PolyConvert* convert_current = nullptr;
 
-		std::vector<p2t::Point*> base_polyline;
-		for (std::vector<glm::vec2>::const_iterator position_it = base_polygon.positions.begin(); position_it != base_polygon.positions.end(); ++position_it)
+		for (std::vector<Polygon>::const_iterator polygon_it = m_Polygons.begin(); polygon_it != m_Polygons.end(); ++polygon_it)
 		{
-			const glm::vec2& position = *position_it;
+			const Polygon& polygon = *polygon_it;
 
-			base_polyline.push_back(
-				new p2t::Point(
-					(double)position.x,
-					(double)position.y
-				)
-			);
-		}
+			bool is_base = false;
 
-		std::vector<const Polygon*> holes;
-
-		if (m_Polygons.size() > 1)
-		{
-			for (std::vector<Polygon>::const_iterator polygon_it = m_Polygons.begin() + 1; polygon_it != m_Polygons.end(); ++polygon_it)
+			if (convert_current != nullptr)
 			{
-				const Polygon& polygon = *polygon_it;
-
-				if (base_polygon.Intersects(polygon))
+				if (convert_current->polygon->Intersects(polygon))
 				{
-					holes.push_back(&polygon);
+					convert_current->holes.push_back(&polygon);
 				}
 				else
 				{
-					for (std::vector<glm::vec2>::const_iterator position_it = polygon.positions.begin(); position_it != polygon.positions.end(); ++position_it)
-					{
-						const glm::vec2& position = *position_it;
-
-						base_polyline.push_back(
-							new p2t::Point(
-								(double)position.x,
-								(double)position.y
-							)
-						);
-					}
+					is_base = true;
 				}
 			}
-		}
-
-		CheckPolylineOverlap(base_polyline);
-
-		p2t::CDT* cdt = new p2t::CDT(base_polyline);
-		if (holes.size() > 0)
-		{
-			for (std::vector<const Polygon*>::iterator hole_it = holes.begin(); hole_it != holes.end(); ++hole_it)
+			else
 			{
-				const Polygon* polygon = *hole_it;
+				is_base = true;
+			}
 
-				if (polygon->positions.size() == 0)
-				{
-					continue;
-				}
+			if (is_base)
+			{
+				convert_current = new PolyConvert;
+				convert_current->polygon = &polygon;
 
-				std::vector<p2t::Point*> hole_polyline;
-				for (std::vector<glm::vec2>::const_iterator position_it = polygon->positions.begin(); position_it != polygon->positions.end(); ++position_it)
+				for (std::vector<glm::vec2>::const_iterator position_it = polygon.positions.begin(); position_it != polygon.positions.end(); ++position_it)
 				{
 					const glm::vec2& position = *position_it;
 
-					hole_polyline.push_back(
+					convert_current->polyline.push_back(
 						new p2t::Point(
 							(double)position.x,
 							(double)position.y
@@ -121,41 +116,78 @@ namespace ExLibris
 					);
 				}
 
-				CheckPolylineOverlap(hole_polyline);
-
-				cdt->AddHole(hole_polyline);
+				converted.push_back(convert_current);
 			}
 		}
 
-		cdt->Triangulate();
+		for (std::vector<PolyConvert*>::iterator convert_it = converted.begin(); convert_it != converted.end(); ++convert_it)
+		{
+			PolyConvert* convert = *convert_it;
 
-		std::vector<p2t::Triangle*> cdt_triangles = cdt->GetTriangles();
+			CheckPolylineOverlap(convert->polyline);
+			convert->cdt = new p2t::CDT(convert->polyline);
 
-		triangles->vertex_count = cdt_triangles.size() * 3;
+			if (convert->holes.size() > 0)
+			{
+				for (std::vector<const Polygon*>::iterator hole_it = convert->holes.begin(); hole_it != convert->holes.end(); ++hole_it)
+				{
+					const Polygon* polygon = *hole_it;
+
+					std::vector<p2t::Point*> hole_polyline;
+					for (std::vector<glm::vec2>::const_iterator position_it = polygon->positions.begin(); position_it != polygon->positions.end(); ++position_it)
+					{
+						const glm::vec2& position = *position_it;
+
+						hole_polyline.push_back(
+							new p2t::Point(
+								(double)position.x,
+								(double)position.y
+							)
+						);
+					}
+
+					CheckPolylineOverlap(hole_polyline);
+
+					convert->cdt->AddHole(hole_polyline);
+				}
+			}
+
+			convert->cdt->Triangulate();
+
+			convert->triangles = convert->cdt->GetTriangles();
+			triangles->vertex_count += convert->triangles.size() * 3;
+		}
 
 		triangles->positions = new glm::vec2[triangles->vertex_count];
 		glm::vec2* dst_position_data = triangles->positions;
 
-		for (std::vector<p2t::Triangle*>::iterator triangle_it = cdt_triangles.begin(); triangle_it != cdt_triangles.end(); ++triangle_it)
+		for (std::vector<PolyConvert*>::iterator convert_it = converted.begin(); convert_it != converted.end(); ++convert_it)
 		{
-			p2t::Triangle* triangle = *triangle_it;
+			PolyConvert* convert = *convert_it;
 
-			p2t::Point* point_a = triangle->GetPoint(0);
-			dst_position_data[0].x = (float)point_a->x;
-			dst_position_data[0].y = (float)point_a->y;
+			for (std::vector<p2t::Triangle*>::iterator triangle_it = convert->triangles.begin(); triangle_it != convert->triangles.end(); ++triangle_it)
+			{
+				p2t::Triangle* triangle = *triangle_it;
 
-			p2t::Point* point_b = triangle->GetPoint(1);
-			dst_position_data[1].x = (float)point_b->x;
-			dst_position_data[1].y = (float)point_b->y;
+				p2t::Point* point_a = triangle->GetPoint(0);
+				dst_position_data[0].x = (float)point_a->x;
+				dst_position_data[0].y = (float)point_a->y;
 
-			p2t::Point* point_c = triangle->GetPoint(2);
-			dst_position_data[2].x = (float)point_c->x;
-			dst_position_data[2].y = (float)point_c->y;
+				p2t::Point* point_b = triangle->GetPoint(1);
+				dst_position_data[1].x = (float)point_b->x;
+				dst_position_data[1].y = (float)point_b->y;
 
-			dst_position_data += 3;
+				p2t::Point* point_c = triangle->GetPoint(2);
+				dst_position_data[2].x = (float)point_c->x;
+				dst_position_data[2].y = (float)point_c->y;
+
+				dst_position_data += 3;
+			}
+
+			delete convert;
 		}
 
-		delete cdt;
+		converted.clear();
 		
 		return triangles;
 	}
