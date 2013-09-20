e05487c0806938d71d017b2472107df1a40e58ae
Anne <knight666+github@gmail.com>
2013-09-20 08:07:35 +0200

    Suite.LineShape: Added test for triangulating a straight line. LineShape: Implemented triangulation.

--- Projects/ExLibrisGL/LineShape.cpp
+++ Projects/ExLibrisGL/LineShape.cpp
@@ -28,9 +28,55 @@ namespace ExLibris
 		m_Polygons.push_back(a_Polygon);
 	}
 
-	TriangleList* LineShape::Triangulate() const
+	TriangleList* LineShape::Triangulate(float a_Thickness) const
 	{
-		return nullptr;
+		const Polygon& polygon = *m_Polygons.begin();
+
+		if (polygon.positions.size() < 2)
+		{
+			return nullptr;
+		}
+
+		float thickness_half = a_Thickness / 2.0f;
+
+		TriangleList* triangles = new TriangleList;
+		triangles->vertex_count = (polygon.positions.size() - 1) * 6;
+		triangles->positions = new glm::vec2[triangles->vertex_count];
+
+		glm::vec2* dst_position = triangles->positions;
+
+		std::vector<glm::vec2>::const_iterator position_it = polygon.positions.begin();
+		std::vector<glm::vec2>::const_iterator position_next_it = position_it + 1;
+		
+		while (position_next_it != polygon.positions.end())
+		{
+			const glm::vec2& current = *position_it++;
+			const glm::vec2& next = *position_next_it++;
+
+			// 1 ---------- 0
+			// |          / |
+			// |       /    |
+			// c .......... n
+			// |    /       |
+			// | /          |
+			// 2 ---------- 3
+
+			glm::vec2 normal = glm::normalize(next - current);
+			glm::vec2 offset_left(normal.y * a_Thickness, -normal.x * thickness_half);
+			glm::vec2 offset_right(-normal.y * a_Thickness, normal.x * thickness_half);
+			
+			dst_position[0] = next + offset_left;
+			dst_position[1] = current + offset_left;
+			dst_position[2] = current + offset_right;
+
+			dst_position[3] = next + offset_left;
+			dst_position[4] = current + offset_right;
+			dst_position[5] = next + offset_right;
+			
+			dst_position += 6;
+		}
+
+		return triangles;
 	}
 
 }; // namespace ExLibris
