d445ea342740095f55bc2431c33d9beb392dc5cb
Anne <knight666@gmail.com>
2013-09-20 08:07:07 +0200

    Polygon: Added overloaded operator to easily add a position. Suite.Polygon: Added test for overloaded operator.

--- Projects/ExLibris.Test/Suite.Polygon.cpp
+++ Projects/ExLibris.Test/Suite.Polygon.cpp
@@ -4,14 +4,25 @@
 
 using namespace ExLibris;
 
+TEST(Polygon, AddPosition)
+{
+	Polygon p;
+
+	p += glm::vec2(25.3f, -92.0f);
+
+	EXPECT_EQ(1, p.positions.size());
+	EXPECT_FLOAT_EQ(25.3f, p.positions[0].x);
+	EXPECT_FLOAT_EQ(-92.0f, p.positions[0].y);
+}
+
 TEST(Polygon, IsPositionInside)
 {
 	Polygon p;
 
-	p.positions.push_back(glm::vec2(-10.0f, -10.0f));
-	p.positions.push_back(glm::vec2(10.0f, -10.0f));
-	p.positions.push_back(glm::vec2(10.0f, 10.0f));
-	p.positions.push_back(glm::vec2(-10.0f, 10.0f));
+	p += glm::vec2(-10.0f, -10.0f);
+	p += glm::vec2(10.0f, -10.0f);
+	p += glm::vec2(10.0f, 10.0f);
+	p += glm::vec2(-10.0f, 10.0f);
 
 	EXPECT_TRUE(p.IsPositionInside(glm::vec2(5.0f, 5.0f)));
 }
@@ -28,13 +39,13 @@ TEST(Polygon, IsPositionInsideConcave)
 	// |               |
 	// o ------------- o
 
-	p.positions.push_back(glm::vec2(-10.0f, -10.0f));
-	p.positions.push_back(glm::vec2( -8.0f, -10.0f));
-	p.positions.push_back(glm::vec2(  0.0f,  -5.0f));
-	p.positions.push_back(glm::vec2(  8.0f, -10.0f));
-	p.positions.push_back(glm::vec2( 10.0f, -10.0f));
-	p.positions.push_back(glm::vec2( 10.0f,  10.0f));
-	p.positions.push_back(glm::vec2(-10.0f,  10.0f));
+	p += glm::vec2(-10.0f, -10.0f);
+	p += glm::vec2( -8.0f, -10.0f);
+	p += glm::vec2(  0.0f,  -5.0f);
+	p += glm::vec2(  8.0f, -10.0f);
+	p += glm::vec2( 10.0f, -10.0f);
+	p += glm::vec2( 10.0f,  10.0f);
+	p += glm::vec2(-10.0f,  10.0f);
 
 	EXPECT_TRUE(p.IsPositionInside(glm::vec2(-9.0f, -8.0f)));
 	EXPECT_FALSE(p.IsPositionInside(glm::vec2( 0.0f, -8.0f)));
@@ -52,8 +63,8 @@ TEST(Polygon, IsPositionInsideNotEnoughVertices)
 {
 	Polygon p;
 
-	p.positions.push_back(glm::vec2(15.0f, 99.8f));
-	p.positions.push_back(glm::vec2(33.5f, 91.7f));
+	p += glm::vec2(15.0f, 99.8f);
+	p += glm::vec2(33.5f, 91.7f);
 
 	EXPECT_FALSE(p.IsPositionInside(glm::vec2(14.0f, 12.0f)));
 }
@@ -61,15 +72,15 @@ TEST(Polygon, IsPositionInsideNotEnoughVertices)
 TEST(Polygon, Intersects)
 {
 	Polygon p_left;
-	p_left.positions.push_back(glm::vec2(-10.0f, -10.0f));
-	p_left.positions.push_back(glm::vec2(10.0f, -10.0f));
-	p_left.positions.push_back(glm::vec2(10.0f, 10.0f));
-	p_left.positions.push_back(glm::vec2(-10.0f, 10.0f));
+	p_left += glm::vec2(-10.0f, -10.0f);
+	p_left += glm::vec2(10.0f, -10.0f);
+	p_left += glm::vec2(10.0f, 10.0f);
+	p_left += glm::vec2(-10.0f, 10.0f);
 
 	Polygon p_right;
-	p_right.positions.push_back(glm::vec2(5.0f, 5.0f));
-	p_right.positions.push_back(glm::vec2(15.0f, 5.0f));
-	p_right.positions.push_back(glm::vec2(15.0f, 15.0f));
+	p_right += glm::vec2(5.0f, 5.0f);
+	p_right += glm::vec2(15.0f, 5.0f);
+	p_right += glm::vec2(15.0f, 15.0f);
 
 	EXPECT_TRUE(p_left.Intersects(p_right));
 	EXPECT_TRUE(p_right.Intersects(p_left));
@@ -78,15 +89,15 @@ TEST(Polygon, Intersects)
 TEST(Polygon, IntersectsNoHit)
 {
 	Polygon p_left;
-	p_left.positions.push_back(glm::vec2(-10.0f, -10.0f));
-	p_left.positions.push_back(glm::vec2(10.0f, -10.0f));
-	p_left.positions.push_back(glm::vec2(10.0f, 10.0f));
-	p_left.positions.push_back(glm::vec2(-10.0f, 10.0f));
+	p_left += glm::vec2(-10.0f, -10.0f);
+	p_left += glm::vec2(10.0f, -10.0f);
+	p_left += glm::vec2(10.0f, 10.0f);
+	p_left += glm::vec2(-10.0f, 10.0f);
 
 	Polygon p_right;
-	p_right.positions.push_back(glm::vec2(0.0f, 25.0f));
-	p_right.positions.push_back(glm::vec2(-15.0f, 50.0f));
-	p_right.positions.push_back(glm::vec2(15.0f, 50.0f));
+	p_right += glm::vec2(0.0f, 25.0f);
+	p_right += glm::vec2(-15.0f, 50.0f);
+	p_right += glm::vec2(15.0f, 50.0f);
 
 	EXPECT_FALSE(p_left.Intersects(p_right));
 	EXPECT_FALSE(p_right.Intersects(p_left));
