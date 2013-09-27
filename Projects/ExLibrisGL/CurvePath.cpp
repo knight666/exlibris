923806cfb58352955eef30ce9e37c10105c93ad7
Anne <knight666@gmail.com>
2013-09-27 17:58:52 +0200

    CurvePath: Added cursor methods. CurvePath: BuildPolygons now uses cursor methods.

--- Projects/ExLibrisGL/CurvePath.cpp
+++ Projects/ExLibrisGL/CurvePath.cpp
@@ -6,9 +6,6 @@ namespace ExLibris
 {
 
 	CurvePath::CurvePath()
-		: m_CurveStarted(false)
-		, m_CurveStep(0)
-		, m_CurveTimeDelta(0.0f)
 	{
 	}
 	
@@ -155,138 +152,168 @@ namespace ExLibris
 		return shapes;
 	}
 
-	glm::vec2 CurvePath::_NextCurvePosition(const CurveSettings& a_Settings)
+	bool CurvePath::StartCursor(const CurveSettings& a_Settings)
+	{
+		if (m_Commands.size() == 0)
+		{
+			return false;
+		}
+
+		m_Cursor.settings = a_Settings;
+		m_Cursor.curve_previous = glm::vec2();
+		m_Cursor.curve_started = false;
+		m_Cursor.curve_step = 0;
+		m_Cursor.curve_time_delta = 0.0f;
+		m_Cursor.position_it = m_Positions.begin();
+		m_Cursor.command_it = m_Commands.begin();
+
+		return true;
+	}
+
+	glm::vec2 CurvePath::ReadCursorPosition()
 	{
 		glm::vec2 position;
 
-		switch (*m_CommandCurrent)
+		if (m_Cursor.command_it == m_Commands.end())
+		{
+			return position;
+		}
+
+		switch (*m_Cursor.command_it)
 		{
 
 		case eCommandType_Move:
 			{
-				position = *m_CommandPositionCurrent++;
-				m_CurveShapeNext = true;
-
-				++m_CommandCurrent;
+				position = *m_Cursor.position_it++;
+				++m_Cursor.command_it;
 
 			} break;
 
 		case eCommandType_Line:
 			{
-				position = *m_CommandPositionCurrent++;
-
-				++m_CommandCurrent;
+				position = *m_Cursor.position_it++;
+				++m_Cursor.command_it;
 
 			} break;
 
 		case eCommandType_CurveConic:
 			{
-				if (m_CurveStarted && m_CurveStep >= a_Settings.precision)
+				if (m_Cursor.curve_started && m_Cursor.curve_step >= m_Cursor.settings.precision)
 				{
-					position = m_CurveTo;
+					position = m_Cursor.curve_to;
 
-					m_CurveStarted = false;
-					m_CurveStep = 0;
+					m_Cursor.curve_started = false;
+					m_Cursor.curve_step = 0;
 
-					++m_CommandCurrent;
+					++m_Cursor.command_it;
 
 					break;
 				}
 
-				if (!m_CurveStarted)
+				if (!m_Cursor.curve_started)
 				{
-					m_CurveFrom = m_CurvePositionPrevious;
-					m_CurveControlA = *m_CommandPositionCurrent++;
-					m_CurveTo = *m_CommandPositionCurrent++;
+					m_Cursor.curve_from = m_Cursor.curve_previous;
+					m_Cursor.curve_control_a = *m_Cursor.position_it++;
+					m_Cursor.curve_to = *m_Cursor.position_it++;
 
-					if (a_Settings.precision <= 1)
+					if (m_Cursor.settings.precision <= 1)
 					{
-						position = m_CurveTo;
+						position = m_Cursor.curve_to;
 
-						++m_CommandCurrent;
+						++m_Cursor.command_it;
 
 						break;
 					}
 
-					m_CurveStep = 1;
-					m_CurveTimeDelta = 1.0f / (float)a_Settings.precision;
+					m_Cursor.curve_step = 1;
+					m_Cursor.curve_time_delta = 1.0f / (float)m_Cursor.settings.precision;
 
-					m_CurveStarted = true;
+					m_Cursor.curve_started = true;
 				}
-				
-				float time = m_CurveStep * m_CurveTimeDelta;
 
-				glm::vec2 ac = glm::mix(m_CurveFrom, m_CurveControlA, time);
-				glm::vec2 cb = glm::mix(m_CurveControlA, m_CurveTo, time);
+				float time = m_Cursor.curve_step * m_Cursor.curve_time_delta;
+
+				glm::vec2 ac = glm::mix(m_Cursor.curve_from, m_Cursor.curve_control_a, time);
+				glm::vec2 cb = glm::mix(m_Cursor.curve_control_a, m_Cursor.curve_to, time);
 				position = glm::mix(ac, cb, time);
 
-				m_CurveStep++;
+				m_Cursor.curve_step++;
 
 			} break;
 
 		case eCommandType_CurveQuadratic:
 			{
-				if (m_CurveStarted && m_CurveStep >= a_Settings.precision)
+				if (m_Cursor.curve_started && m_Cursor.curve_step >= m_Cursor.settings.precision)
 				{
-					position = m_CurveTo;
+					position = m_Cursor.curve_to;
 
-					m_CurveStarted = false;
-					m_CurveStep = 0;
+					m_Cursor.curve_started = false;
+					m_Cursor.curve_step = 0;
 
-					++m_CommandCurrent;
+					++m_Cursor.command_it;
 
 					break;
 				}
 
-				if (!m_CurveStarted)
+				if (!m_Cursor.curve_started)
 				{
-					m_CurveFrom = m_CurvePositionPrevious;
-					m_CurveControlA = *m_CommandPositionCurrent++;
-					m_CurveControlB = *m_CommandPositionCurrent++;
-					m_CurveTo = *m_CommandPositionCurrent++;
+					m_Cursor.curve_from = m_Cursor.curve_previous;
+					m_Cursor.curve_control_a = *m_Cursor.position_it++;
+					m_Cursor.curve_control_b = *m_Cursor.position_it++;
+					m_Cursor.curve_to = *m_Cursor.position_it++;
 
-					if (a_Settings.precision <= 1)
+					if (m_Cursor.settings.precision <= 1)
 					{
-						position = m_CurveTo;
+						position = m_Cursor.curve_to;
 
-						++m_CommandCurrent;
+						++m_Cursor.command_it;
 
 						break;
 					}
 
-					m_CurveStep = 1;
-					m_CurveTimeDelta = 1.0f / (float)a_Settings.precision;
+					m_Cursor.curve_step = 1;
+					m_Cursor.curve_time_delta = 1.0f / (float)m_Cursor.settings.precision;
 
-					m_CurveStarted = true;
+					m_Cursor.curve_started = true;
 				}
 
-				float time = m_CurveStep * m_CurveTimeDelta;
+				float time = m_Cursor.curve_step * m_Cursor.curve_time_delta;
 
-				glm::vec2 ab = glm::mix(m_CurveFrom, m_CurveControlA, time);
-				glm::vec2 bc = glm::mix(m_CurveControlA, m_CurveControlB, time);
-				glm::vec2 cd = glm::mix(m_CurveControlB, m_CurveTo, time);
+				glm::vec2 ab = glm::mix(m_Cursor.curve_from, m_Cursor.curve_control_a, time);
+				glm::vec2 bc = glm::mix(m_Cursor.curve_control_a, m_Cursor.curve_control_b, time);
+				glm::vec2 cd = glm::mix(m_Cursor.curve_control_b, m_Cursor.curve_to, time);
 
 				glm::vec2 mixed_a = glm::mix(ab, bc, time);
 				glm::vec2 mixed_b = glm::mix(bc, cd, time);
 
 				position = glm::mix(mixed_a, mixed_b, time);
 
-				m_CurveStep++;
+				m_Cursor.curve_step++;
 
 			} break;
 
 		}
 
-		m_CurvePositionPrevious = position;
+		m_Cursor.curve_previous = position;
 		
 		return position;
+	}
+
+	bool CurvePath::IsCursorNextShape() const
+	{
+		return ((m_Cursor.command_it != m_Commands.begin()) && (*m_Cursor.command_it == eCommandType_Move));
+	}
+
+	bool CurvePath::IsCursorValid() const
+	{
+		return ((m_Commands.size() > 0) && (m_Cursor.command_it != m_Commands.end()));
 	}
 
 	std::vector<Polygon> CurvePath::BuildPolygons(const CurveSettings& a_Settings)
 	{
 		std::vector<Polygon> shapes;
 
-		if (m_Commands.size() == 0)
+		if (!StartCursor(a_Settings))
 		{
 			return shapes;
 		}
@@ -296,16 +323,9 @@ namespace ExLibris
 
 		Polygon* shape_current = &shapes.back();
 
-		m_CommandPositionCurrent = m_Positions.begin();
-		m_CommandCurrent = m_Commands.begin();
-
-		glm::vec2 position;
-
-		while (m_CommandCurrent != m_Commands.end())
+		while (IsCursorValid())
 		{
-			// check if it's actually a new shape and not the first move command
-
-			if (m_CommandCurrent != m_Commands.begin() && *m_CommandCurrent == eCommandType_Move)
+			if (IsCursorNextShape())
 			{
 				Polygon shape;
 				shapes.push_back(shape);
@@ -313,7 +333,7 @@ namespace ExLibris
 				shape_current = &shapes.back();
 			}
 
-			*shape_current += _NextCurvePosition(a_Settings);
+			*shape_current += ReadCursorPosition();
 		}
 
 		return shapes;
