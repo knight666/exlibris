041341f192611f45e7563fa2266e634f9d45156d
Anne <knight666+github@gmail.com>
2013-10-01 15:19:15 +0200

    MeshOpenGL: Moved implementation to its own header.

---
+++ Projects/FrameworkGL/MeshOpenGL.h
@@ -0,0 +1,39 @@
+#ifndef _MESHOPENGL_H_
+#define _MESHOPENGL_H_
+
+#include <GL/glew.h>
+
+#include <IMeshVisitor.h>
+
+namespace Framework
+{
+
+	class MeshOpenGL
+		: public ExLibris::IMeshVisitor
+	{
+
+	public:
+
+		MeshOpenGL();
+		~MeshOpenGL();
+
+		ExLibris::TriangleOrientation GetOrientation() const;
+
+		void VisitBuilderMeshBegin(unsigned int a_VertexCount);
+		void VisitBuilderTriangle(const glm::vec2& a_A, const glm::vec2& a_B, const glm::vec2& a_C);
+		void VisitBuilderMeshEnd();
+
+		void Render();
+
+	private:
+
+		GLuint m_Buffer;
+		glm::vec2* m_VertexData;
+		size_t m_VertexCount;
+		size_t m_VertexFilled;
+
+	}; // class MeshOpenGL
+
+}; // namespace Framework
+
+#endif
