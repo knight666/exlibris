d6e5c109017a4f3332ab1391d25c3bc14d3bfa40
Anne <knight666+github@gmail.com>
2013-10-03 17:54:40 +0200

    LineShape: Added BuildFilledMesh method. LineShape: Renamed BuildMesh method to BuildOutlineMesh. ExampleFontOutline: Text is drawn filled. ExampleFontOutline: Zoom speed is slowed.

--- Projects/ExLibrisGL/LineShape.cpp
+++ Projects/ExLibrisGL/LineShape.cpp
@@ -250,29 +250,202 @@ namespace ExLibris
 		return triangles;
 	}
 
-	MeshBuilder* LineShape::BuildMesh(const LineMeshOptions& a_Options)
+	MeshBuilder* LineShape::BuildOutlineMesh(const LineMeshOptions& a_Options) const
 	{
 		MeshBuilder* builder = new MeshBuilder;
 
+		if (m_Polygons.size() == 0)
+		{
+			return builder;
+		}
+
 		if (a_Options.quality == LineMeshOptions::eQuality_Fast)
 		{
-			for (std::vector<Polygon>::iterator polygon_it = m_Polygons.begin(); polygon_it != m_Polygons.end(); ++polygon_it)
+			for (std::vector<Polygon>::const_iterator polygon_it = m_Polygons.begin(); polygon_it != m_Polygons.end(); ++polygon_it)
 			{
-				_TriangulatePolygonFast(builder, *polygon_it, a_Options.thickness);
+				const Polygon& polygon = *polygon_it;
+
+				if (polygon.positions.size() > 2)
+				{
+					_TriangulateOutlineFast(builder, polygon, a_Options.thickness);
+				}
 			}
 		}
 		else if (a_Options.quality == LineMeshOptions::eQuality_Gapless)
 		{
-			for (std::vector<Polygon>::iterator polygon_it = m_Polygons.begin(); polygon_it != m_Polygons.end(); ++polygon_it)
+			for (std::vector<Polygon>::const_iterator polygon_it = m_Polygons.begin(); polygon_it != m_Polygons.end(); ++polygon_it)
 			{
-				_TriangulatePolygonGapless(builder, *polygon_it, a_Options.thickness);
+				const Polygon& polygon = *polygon_it;
+
+				if (polygon.positions.size() > 2)
+				{
+					_TriangulateOutlineGapless(builder, polygon, a_Options.thickness);
+				}
 			}
 		}
 		
 		return builder;
 	}
 
-	void LineShape::_TriangulatePolygonFast(MeshBuilder* a_Target, const Polygon& a_Polygon, float a_Thickness)
+	struct PolyConvert
+	{
+		PolyConvert()
+			: polygon(nullptr)
+			, cdt(nullptr)
+		{
+		}
+
+		~PolyConvert()
+		{
+			if (cdt != nullptr)
+			{
+				delete cdt;
+				cdt = nullptr;
+			}
+		}
+
+		const Polygon* polygon;
+		std::vector<p2t::Point*> polyline;
+		std::vector<const Polygon*> holes;
+		p2t::CDT* cdt;
+		std::vector<p2t::Triangle*> triangles;
+	};
+
+	MeshBuilder* LineShape::BuildFilledMesh() const
+	{
+		MeshBuilder* builder = new MeshBuilder;
+
+		if (m_Polygons.size() == 0)
+		{
+			return builder;
+		}
+
+		std::vector<PolyConvert*> converted;
+		PolyConvert* convert_current = nullptr;
+
+		for (std::vector<Polygon>::const_iterator polygon_it = m_Polygons.begin(); polygon_it != m_Polygons.end(); ++polygon_it)
+		{
+			const Polygon& polygon = *polygon_it;
+
+			bool is_base = false;
+
+			if (convert_current != nullptr)
+			{
+				if (convert_current->polygon->Intersects(polygon))
+				{
+					convert_current->holes.push_back(&polygon);
+				}
+				else
+				{
+					is_base = true;
+				}
+			}
+			else
+			{
+				is_base = true;
+			}
+
+			if (is_base)
+			{
+				convert_current = new PolyConvert;
+				convert_current->polygon = &polygon;
+
+				for (std::vector<glm::vec2>::const_iterator position_it = polygon.positions.begin(); position_it != polygon.positions.end(); ++position_it)
+				{
+					const glm::vec2& position = *position_it;
+
+					convert_current->polyline.push_back(
+						new p2t::Point(
+							(double)position.x,
+							(double)position.y
+						)
+					);
+				}
+
+				converted.push_back(convert_current);
+			}
+		}
+
+		for (std::vector<PolyConvert*>::iterator convert_it = converted.begin(); convert_it != converted.end(); ++convert_it)
+		{
+			PolyConvert* convert = *convert_it;
+
+			_CheckPolylineOverlap(convert->polyline);
+			convert->cdt = new p2t::CDT(convert->polyline);
+
+			if (convert->holes.size() > 0)
+			{
+				for (std::vector<const Polygon*>::iterator hole_it = convert->holes.begin(); hole_it != convert->holes.end(); ++hole_it)
+				{
+					const Polygon* polygon = *hole_it;
+
+					std::vector<p2t::Point*> hole_polyline;
+					for (std::vector<glm::vec2>::const_iterator position_it = polygon->positions.begin(); position_it != polygon->positions.end(); ++position_it)
+					{
+						const glm::vec2& position = *position_it;
+
+						hole_polyline.push_back(
+							new p2t::Point(
+								(double)position.x,
+								(double)position.y
+							)
+						);
+					}
+
+					_CheckPolylineOverlap(hole_polyline);
+
+					convert->cdt->AddHole(hole_polyline);
+				}
+			}
+
+			convert->cdt->Triangulate();
+			convert->triangles = convert->cdt->GetTriangles();
+		}
+
+		for (std::vector<PolyConvert*>::iterator convert_it = converted.begin(); convert_it != converted.end(); ++convert_it)
+		{
+			PolyConvert* convert = *convert_it;
+
+			for (std::vector<p2t::Triangle*>::iterator triangle_it = convert->triangles.begin(); triangle_it != convert->triangles.end(); ++triangle_it)
+			{
+				p2t::Triangle* triangle = *triangle_it;
+
+				p2t::Point* point_a = triangle->GetPoint(0);
+				p2t::Point* point_b = triangle->GetPoint(1);
+				p2t::Point* point_c = triangle->GetPoint(2);
+
+				glm::vec2 a((float)point_a->x, (float)point_a->y);
+				glm::vec2 b((float)point_b->x, (float)point_b->y);
+				glm::vec2 c((float)point_c->x, (float)point_c->y);
+
+				//builder->AddTriangle(a, b, c);
+				builder->AddTriangle(c, b, a);
+			}
+
+			delete convert;
+		}
+
+		converted.clear();
+
+		return builder;
+	}
+
+	void LineShape::_CheckPolylineOverlap(std::vector<p2t::Point*>& a_Polyline) const
+	{
+		// check if last and first position overlap
+
+		p2t::Point* position_first = a_Polyline.front();
+		p2t::Point* position_last = a_Polyline.back();
+
+		double delta = 1e-6;
+		if (abs(position_first->x - position_last->x) < delta && abs(position_first->y - position_last->y) < delta)
+		{
+			delete position_last;
+			a_Polyline.pop_back();
+		}
+	}
+
+	void LineShape::_TriangulateOutlineFast(MeshBuilder* a_Target, const Polygon& a_Polygon, float a_Thickness) const
 	{
 		if (a_Polygon.positions.size() < 2)
 		{
@@ -297,7 +470,7 @@ namespace ExLibris
 		}
 	}
 
-	void LineShape::_TriangulatePolygonGapless(MeshBuilder* a_Target, const Polygon& a_Polygon, float a_Thickness)
+	void LineShape::_TriangulateOutlineGapless(MeshBuilder* a_Target, const Polygon& a_Polygon, float a_Thickness) const
 	{
 		if (a_Polygon.positions.size() < 2)
 		{
