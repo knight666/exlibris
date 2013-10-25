96d94c11a171fa65f4b9bc161655f15c382d6427
Anne <knight666+github@gmail.com>
2013-10-25 18:00:56 +0200

    Moved font face loading tests to Suite.FontLoading.

--- Projects/ExLibris.Test/Suite.FontLoading.cpp
+++ Projects/ExLibris.Test/Suite.FontLoading.cpp
@@ -1,6 +1,7 @@
 #include "ExLibris.Test.PCH.h"
 
 #include <Family.h>
+#include <FontFace.h>
 #include <FontLoaderFreetype.h>
 #include <Library.h>
 
@@ -24,4 +25,73 @@ TEST(FontLoaderFreetype, LoadFontFileNotFound)
 
 	IFont* font_loaded = loader->LoadFont("Fonts/Arial.ttf");
 	EXPECT_EQ(nullptr, font_loaded);
+}
+
+class FaceContext
+	: public ::testing::Test
+{
+
+public:
+
+	void SetUp()
+	{
+		library = new Library;
+		library->AddLoader(new FontLoaderFreetype(library));
+
+		font = (FontFreetype*)library->LoadFont("Fonts/Roboto/Roboto-Regular.ttf");
+
+		FaceOptions options;
+		options.size = 24.0f;
+
+		face = font->CreateFace(options);
+	}
+
+	void TearDown()
+	{
+		delete library;
+	}
+
+protected:
+
+	Library* library;
+	FontFreetype* font;
+	FontFace* face;
+
+};
+
+TEST_F(FaceContext, LineHeight)
+{
+	EXPECT_FLOAT_EQ(24.0f, face->GetSize());
+	EXPECT_FLOAT_EQ(42.0f, face->GetLineHeight());
+}
+
+TEST_F(FaceContext, FindGlyph)
+{
+	Glyph* glyph = face->FindGlyph((unsigned int)'6');
+	ASSERT_NE(nullptr, glyph);
+
+	EXPECT_EQ(25, glyph->index);
+	ASSERT_NE(nullptr, glyph->metrics);
+	EXPECT_FLOAT_EQ(18.0f, glyph->metrics->advance);
+	EXPECT_VEC2_EQ(2.0f, 19.0f, glyph->metrics->offset);
+	EXPECT_VEC2_EQ(2.0625000f, 0.0000000f, glyph->metrics->bounding_box.minimum);
+	EXPECT_VEC2_EQ(16.859375f, 23.000000f, glyph->metrics->bounding_box.maximum);
+}
+
+TEST_F(FaceContext, FindGlyphOutline)
+{
+	Glyph* glyph = face->FindGlyph((unsigned int)'-');
+	ASSERT_NE(nullptr, glyph);
+
+	ASSERT_NE(nullptr, glyph->outline);
+	EXPECT_EQ(5, glyph->outline->GetCommandCount());
+
+	EXPECT_EQ(5, glyph->outline->GetPositionCount());
+	EXPECT_VEC2_EQ(8.2031250f, -8.5937500f, glyph->outline->GetPosition(0));
+}
+
+TEST_F(FaceContext, FindGlyphNotFound)
+{
+	Glyph* glyph = face->FindGlyph(0x777126);
+	ASSERT_EQ(nullptr, glyph);
 }
