6283a8926395ef8dd094db6a4d4a165a3feb06d4
Anne <knight666+github@gmail.com>
2013-11-14 09:57:09 +0100

    IGlyphProvider: Added weight, style and line properties.

--- Projects/ExLibrisGL/IGlyphProvider.h
+++ Projects/ExLibrisGL/IGlyphProvider.h
@@ -39,9 +39,47 @@ namespace ExLibris
 	
 	public:
 	
+		IGlyphProvider()
+			: m_Weight(eWeight_Normal)
+			, m_Style(eStyle_None)
+			, m_LineHeight(0.0f)
+			, m_Ascent(0.0f)
+			, m_Descent(0.0f)
+		{
+		}
+
 		virtual ~IGlyphProvider()
 		{
 		}
+
+	public:
+
+		Weight GetWeight() const
+		{
+			return m_Weight;
+		}
+
+		Style GetStyle() const
+		{
+			return m_Style;
+		}
+
+		float GetLineHeight() const
+		{
+			return m_LineHeight;
+		}
+
+		float GetAscent() const
+		{
+			return m_Ascent;
+		}
+
+		float GetDescent() const
+		{
+			return m_Descent;
+		}
+
+	public:
 
 		virtual bool SetSize(float a_Size) = 0;
 	
@@ -52,6 +90,14 @@ namespace ExLibris
 		virtual GlyphBitmap* CreateBitmap(int a_Codepoint) = 0;
 
 		virtual CurvePath* CreateOutline(int a_Codepoint) = 0;
+
+	protected:
+
+		Weight m_Weight;
+		Style m_Style;
+		float m_LineHeight;
+		float m_Ascent;
+		float m_Descent;
 	
 	}; // class IGlyphProvider
 
