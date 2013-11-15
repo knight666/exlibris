a98092fc42d732180d5880f3fe7877446a8f008c
Anne <knight666+github@gmail.com>
2013-11-15 13:15:06 +0100

    Family: Added management of glyph providers. Suite.Family: Added tests for adding and finding glyph providers.

--- Projects/ExLibris.Test/ExLibris.Test.PCH.h
+++ Projects/ExLibris.Test/ExLibris.Test.PCH.h
@@ -5,6 +5,7 @@
 
 #include <fstream>
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
--- Projects/ExLibrisGL/ExLibrisGL.PCH.h
+++ Projects/ExLibrisGL/ExLibrisGL.PCH.h
@@ -29,6 +29,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <set>
 #include <sstream>
 #include <string>
 #include <vector>
