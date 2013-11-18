fc66c389c39bb5b0bccc734973b58f5dfd375502
Anne <knight666@gmail.com>
2013-11-18 16:53:17 +0100

    Family: Removed font methods. Suite.Family: Removed font tests. Suite.Family: Added construct test.

--- Projects/ExLibris.Test/Suite.Family.cpp
+++ Projects/ExLibris.Test/Suite.Family.cpp
@@ -2,142 +2,16 @@
 
 #include <Family.h>
 
-#include "Mock.Font.h"
 #include "Mock.GlyphProvider.h"
 
 using namespace ExLibris;
 
-TEST(Family, AddFont)
+TEST(Family, Construct)
 {
 	Family* fam = new Family(nullptr, "Kittykat");
 
-	MockFont* font = new MockFont(fam);
-	fam->AddFont(font);
-
-	EXPECT_EQ(1, fam->GetFontCount());
-}
-
-TEST(Family, AddFontTwice)
-{
-	Family* fam = new Family(nullptr, "Bangers 'N Mash");
-
-	MockFont* font = new MockFont(fam);
-	fam->AddFont(font);
-	fam->AddFont(font);
-
-	EXPECT_EQ(1, fam->GetFontCount());
-}
-
-TEST(Family, FindFont)
-{
-	Family* fam = new Family(nullptr, "Veranda");
-
-	MockFont* font = new MockFont(fam);
-	font->SetWeight(eWeight_Bold);
-	font->SetStyle(eStyle_None);
-
-	fam->AddFont(font);
-
-	IFont* font_found = fam->FindFont(eWeight_Bold, eStyle_None);
-	EXPECT_EQ(font, font_found);
-}
-
-TEST(Family, FindFontMatchWeight)
-{
-	Family* fam = new Family(nullptr, "Droned");
-
-	MockFont* font = new MockFont(fam);
-	font->SetWeight(eWeight_Bold);
-	font->SetStyle(eStyle_Italicized);
-
-	fam->AddFont(font);
-
-	IFont* font_found = fam->FindFont(eWeight_Bold, eStyle_None);
-	EXPECT_EQ(font, font_found);
-}
-
-TEST(Family, FindFontMatchStyle)
-{
-	Family* fam = new Family(nullptr, "BigBoned");
-
-	MockFont* font = new MockFont(fam);
-	font->SetWeight(eWeight_Bold);
-	font->SetStyle(eStyle_Italicized);
-
-	fam->AddFont(font);
-
-	IFont* font_found = fam->FindFont(eWeight_Normal, eStyle_Italicized);
-	EXPECT_EQ(font, font_found);
-}
-
-TEST(Family, FindFontBestMatchWeight)
-{
-	Family* fam = new Family(nullptr, "Racketeer");
-
-	MockFont* font_first = new MockFont(fam);
-	font_first->SetWeight(eWeight_Normal);
-	font_first->SetStyle(eStyle_None);
-	fam->AddFont(font_first);
-
-	MockFont* font_second = new MockFont(fam);
-	font_second->SetWeight(eWeight_Bold);
-	font_second->SetStyle(eStyle_None);
-	fam->AddFont(font_second);
-
-	IFont* font_found = fam->FindFont(eWeight_Bold, eStyle_None);
-	EXPECT_EQ(font_second, font_found);
-}
-
-TEST(Family, FindFontBestMatchStyle)
-{
-	Family* fam = new Family(nullptr, "Hairy Head");
-
-	MockFont* font_first = new MockFont(fam);
-	font_first->SetWeight(eWeight_Normal);
-	font_first->SetStyle(eStyle_Italicized);
-	fam->AddFont(font_first);
-
-	MockFont* font_second = new MockFont(fam);
-	font_second->SetWeight(eWeight_Normal);
-	font_second->SetStyle(eStyle_None);
-	fam->AddFont(font_second);
-
-	IFont* font_found = fam->FindFont(eWeight_Normal, eStyle_Italicized);
-	EXPECT_EQ(font_first, font_found);
-}
-
-TEST(Family, FindFontPartialMatch)
-{
-	Family* fam = new Family(nullptr, "Ringading");
-
-	MockFont* font = new MockFont(fam);
-	font->SetWeight(eWeight_Bold);
-	font->SetStyle(eStyle_None);
-	fam->AddFont(font);
-
-	IFont* font_found = fam->FindFont(eWeight_Bold, eStyle_Italicized);
-	EXPECT_EQ(font, font_found);
-}
-
-TEST(Family, FindFontNoMatch)
-{
-	Family* fam = new Family(nullptr, "Destroyer of Cookies");
-
-	MockFont* font = new MockFont(fam);
-	font->SetWeight(eWeight_Normal);
-	font->SetStyle(eStyle_None);
-	fam->AddFont(font);
-
-	IFont* font_found = fam->FindFont(eWeight_Bold, eStyle_Italicized);
-	EXPECT_EQ(nullptr, font_found);
-}
-
-TEST(Family, FindFontEmpty)
-{
-	Family* fam = new Family(nullptr, "Teacup");
-
-	IFont* font_found = fam->FindFont(eWeight_Normal, eStyle_Italicized);
-	EXPECT_EQ(nullptr, font_found);
+	EXPECT_STREQ("Kittykat", fam->GetName().c_str());
+	EXPECT_EQ(0, fam->GetGlyphProviderCount());
 }
 
 TEST(Family, AddGlyphProvider)
--- Projects/ExLibrisGL/Family.cpp
+++ Projects/ExLibrisGL/Family.cpp
@@ -39,12 +39,6 @@ namespace ExLibris
 	
 	Family::~Family()
 	{
-		for (std::set<IFont*>::iterator font_it = m_Fonts.begin(); font_it != m_Fonts.end(); ++font_it)
-		{
-			delete *font_it;
-		}
-		m_Fonts.clear();
-
 		for (std::set<IGlyphProvider*>::iterator provider_it = m_GlyphProviders.begin(); provider_it != m_GlyphProviders.end(); ++provider_it)
 		{
 			delete *provider_it;
@@ -60,51 +54,6 @@ namespace ExLibris
 	const std::string& Family::GetName() const
 	{
 		return m_Name;
-	}
-
-	size_t Family::GetFontCount() const
-	{
-		return m_Fonts.size();
-	}
-
-	void Family::AddFont(IFont* a_Font)
-	{
-		std::set<IFont*>::iterator found = m_Fonts.find(a_Font);
-		if (found == m_Fonts.end())
-		{
-			m_Fonts.insert(a_Font);
-		}
-	}
-
-	IFont* Family::FindFont(Weight a_Weight, Style a_Style) const
-	{
-		IFont* match = nullptr;
-
-		for (std::set<IFont*>::const_iterator font_it = m_Fonts.begin(); font_it != m_Fonts.end(); ++font_it)
-		{
-			IFont* font = *font_it;
-
-			int score = 0;
-
-			if (font->GetStyle() == a_Style)
-			{
-				match = font;
-				score++;
-			}
-
-			if (font->GetWeight() == a_Weight)
-			{
-				match = font;
-				score++;
-			}
-
-			if (score == 2)
-			{
-				break;
-			}
-		}
-
-		return match;
 	}
 
 	size_t Family::GetGlyphProviderCount() const
