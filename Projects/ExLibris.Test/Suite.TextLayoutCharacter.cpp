f3a3ca85f545e828ef0741ea9e00d624924eabe0
Anne <knight666@gmail.com>
2013-11-26 09:18:24 +0100

    TextLayoutCharacter: Class for holding characters in a layout. Suite.TextLayoutCharacter: Tests for layout characters. ITextLayoutElement: Base class for layout elements.

---
+++ Projects/ExLibris.Test/Suite.TextLayoutCharacter.cpp
@@ -0,0 +1,68 @@
+#include "ExLibris.Test.PCH.h"
+
+#include <Face.h>
+#include <TextLayoutCharacter.h>
+
+#include "Mock.GlyphProvider.h"
+
+using namespace ExLibris;
+
+TEST(TextLayoutCharacter, Construct)
+{
+	MockGlyphProvider provider(nullptr);
+	Face* face = provider.CreateFace(15.0f);
+
+	TextLayoutCharacter* character = new TextLayoutCharacter(face, 'A', face->CreateMetrics('A'));
+
+	EXPECT_VEC2_EQ(0.0f, 0.0f, character->GetLayoutGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(0.0f, 0.0f, character->GetLayoutGeometry().GetDimensions());
+	EXPECT_VEC2_EQ(0.0f, 0.0f, character->GetElementGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(0.0f, 0.0f, character->GetElementGeometry().GetDimensions());
+}
+
+TEST(TextLayoutCharacter, CalculateGeometry)
+{
+	MockGlyphProvider provider(nullptr);
+	Face* face = provider.CreateFace(24.0f);
+
+	TextLayoutCharacter* character = new TextLayoutCharacter(face, '7', face->CreateMetrics('7'));
+
+	character->CalculateGeometry();
+
+	EXPECT_VEC2_EQ(0.0f, 0.0f - (24.0f / 2.0f) + -(24.0f / 3.0f), character->GetLayoutGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(12.0f, 24.0f * 1.5f, character->GetLayoutGeometry().GetDimensions());
+	EXPECT_VEC2_EQ(2.0f, 2.0f, character->GetElementGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(8.0f, 8.0f, character->GetElementGeometry().GetDimensions());
+}
+
+TEST(TextLayoutCharacter, CalculateGeometryWithPosition)
+{
+	MockGlyphProvider provider(nullptr);
+	Face* face = provider.CreateFace(24.0f);
+
+	TextLayoutCharacter* character = new TextLayoutCharacter(face, 'P', face->CreateMetrics('P'));
+	character->SetPosition(glm::vec2(51.0f, 12.0f));
+
+	character->CalculateGeometry();
+
+	EXPECT_VEC2_EQ(51.0f, 12.0f - (24.0f / 2.0f) + -(24.0f / 3.0f), character->GetLayoutGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(12.0f, 24.0f * 1.5f, character->GetLayoutGeometry().GetDimensions());
+	EXPECT_VEC2_EQ(51.0f + 2.0f, 12.0f + 2.0f, character->GetElementGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(8.0f, 8.0f, character->GetElementGeometry().GetDimensions());
+}
+
+TEST(TextLayoutCharacter, CalculateGeometryWithKerningAdjustment)
+{
+	MockGlyphProvider provider(nullptr);
+	Face* face = provider.CreateFace(33.0f);
+
+	TextLayoutCharacter* character = new TextLayoutCharacter(face, '%', face->CreateMetrics('%'));
+	character->SetKerningAdjustment(glm::vec2(-2.4f, 15.2f));
+
+	character->CalculateGeometry();
+
+	EXPECT_VEC2_EQ(0.0f, 0.0f - (33.0f / 2.0f) + -(33.0f / 3.0f), character->GetLayoutGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(12.0f - 2.4f, 33.0f * 1.5f, character->GetLayoutGeometry().GetDimensions());
+	EXPECT_VEC2_EQ(2.0f, 2.0f, character->GetElementGeometry().GetTopLeft());
+	EXPECT_VEC2_EQ(8.0f, 8.0f, character->GetElementGeometry().GetDimensions());
+}
---
+++ Projects/ExLibrisGL/TextLayoutCharacter.h
@@ -0,0 +1,67 @@
+/*
+ * This file is a part of the ExLibris project.
+ *
+ * Copyright (C) 2013 Quinten Lansu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal 
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include "ITextLayoutElement.h"
+
+namespace ExLibris
+{
+	struct GlyphMetrics;
+}
+
+namespace ExLibris
+{
+
+	class TextLayoutCharacter
+		: public ITextLayoutElement
+
+	{
+	
+	public:
+	
+		TextLayoutCharacter(Face* a_Face, unsigned int a_GlyphIndex, GlyphMetrics* a_Metrics);
+		~TextLayoutCharacter();
+	
+		void SetPosition(const glm::vec2& a_Position);
+
+		void SetKerningAdjustment(const glm::vec2& a_Adjustment);
+
+	private:
+
+		void OnGeometryCalculated();
+		void OnAccepted(ITextLayoutVisitor& a_Visitor);
+
+	private:
+
+		Face* m_Face;
+		unsigned int m_GlyphIndex;
+		GlyphMetrics* m_Metrics;
+		glm::vec2 m_Position;
+		glm::vec2 m_Adjustment;
+
+	
+	}; // class TextLayoutCharacter
+
+}; // namespace ExLibris
