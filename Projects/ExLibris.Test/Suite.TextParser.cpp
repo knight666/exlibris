733d09550b63374ea96919ca659516b91195156e
Anne <knight666@gmail.com>
2013-12-06 16:52:32 +0100

    Suite.TextParser: Added tests for changing text format.

--- Projects/ExLibris.Test/Suite.TextParser.cpp
+++ Projects/ExLibris.Test/Suite.TextParser.cpp
@@ -187,3 +187,117 @@ TEST(TextParser, ReadSentence)
 		EXPECT_EQ(TextParserToken::eCodepointType_Unprintable, token.type);
 	}
 }
+
+TEST(TextParser, SetFamilyName)
+{
+	MockTextParser parser;
+
+	TextParserToken token;
+	token.changes |= TextParserToken::eChanged_FamilyName;
+	token.family_name = "Brillo";
+
+	parser.AddTokenStyle(token);
+
+	EXPECT_TRUE(parser.ReadToken());
+	{
+		const TextParserToken& token = parser.GetToken();
+
+		EXPECT_EQ(TextParserToken::eChanged_FamilyName, token.changes);
+		EXPECT_STREQ("Brillo", token.family_name.c_str());
+	}
+}
+
+TEST(TextParser, SetSize)
+{
+	MockTextParser parser;
+
+	TextParserToken token;
+	token.changes |= TextParserToken::eChanged_Size;
+	token.size = 16.8f;
+
+	parser.AddTokenStyle(token);
+
+	EXPECT_TRUE(parser.ReadToken());
+	{
+		const TextParserToken& token = parser.GetToken();
+
+		EXPECT_EQ(TextParserToken::eChanged_Size, token.changes);
+		EXPECT_FLOAT_EQ(16.8f, token.size);
+	}
+}
+
+TEST(TextParser, SetWeight)
+{
+	MockTextParser parser;
+
+	TextParserToken token;
+	token.changes |= TextParserToken::eChanged_Weight;
+	token.weight = eWeight_Bold;
+
+	parser.AddTokenStyle(token);
+
+	EXPECT_TRUE(parser.ReadToken());
+	{
+		const TextParserToken& token = parser.GetToken();
+
+		EXPECT_EQ(TextParserToken::eChanged_Weight, token.changes);
+		EXPECT_EQ(eWeight_Bold, token.weight);
+	}
+}
+
+TEST(TextParser, SetStyle)
+{
+	MockTextParser parser;
+
+	TextParserToken token;
+	token.changes |= TextParserToken::eChanged_Style;
+	token.style = eStyle_Italicized;
+
+	parser.AddTokenStyle(token);
+
+	EXPECT_TRUE(parser.ReadToken());
+	{
+		const TextParserToken& token = parser.GetToken();
+
+		EXPECT_EQ(TextParserToken::eChanged_Style, token.changes);
+		EXPECT_EQ(eStyle_Italicized, token.style);
+	}
+}
+
+TEST(TextParser, SetColor)
+{
+	MockTextParser parser;
+
+	TextParserToken token;
+	token.changes |= TextParserToken::eChanged_Color;
+	token.color = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+
+	parser.AddTokenStyle(token);
+
+	EXPECT_TRUE(parser.ReadToken());
+	{
+		const TextParserToken& token = parser.GetToken();
+
+		EXPECT_EQ(TextParserToken::eChanged_Color, token.changes);
+		EXPECT_COLOR_EQ(1.0f, 0.0f, 0.0f, 1.0f, token.color);
+	}
+}
+
+TEST(TextParser, SetBackgroundColor)
+{
+	MockTextParser parser;
+
+	TextParserToken token;
+	token.changes |= TextParserToken::eChanged_BackgroundColor;
+	token.background_color = glm::vec4(0.8f, 0.8f, 0.8f, 1.0f);
+
+	parser.AddTokenStyle(token);
+
+	EXPECT_TRUE(parser.ReadToken());
+	{
+		const TextParserToken& token = parser.GetToken();
+
+		EXPECT_EQ(TextParserToken::eChanged_BackgroundColor, token.changes);
+		EXPECT_COLOR_EQ(0.8f, 0.8f, 0.8f, 1.0f, token.background_color);
+	}
+}
