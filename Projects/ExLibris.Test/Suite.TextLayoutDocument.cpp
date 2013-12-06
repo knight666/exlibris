6e0d29e82ae676e00eeb6a424d801b176de40697
Anne <knight666+github@gmail.com>
2013-12-06 10:23:11 +0100

    Suite.TextLayoutDocument: Added context for tests.

--- Projects/ExLibris.Test/Suite.TextLayoutDocument.cpp
+++ Projects/ExLibris.Test/Suite.TextLayoutDocument.cpp
@@ -1,5 +1,6 @@
 #include "ExLibris.Test.PCH.h"
 
+#include <Family.h>
 #include <FontLoaderFreetype.h>
 #include <Library.h>
 #include <TextFormat.h>
@@ -8,7 +9,93 @@
 #include <TextLayoutLine.h>
 #include <TextParserMarkdown.h>
 
+#include "Mock.GlyphProvider.h"
+
 using namespace ExLibris;
+
+class TextLayoutDocumentContext
+	: public ::testing::Test
+{
+
+public:
+
+	void SetUp()
+	{
+		library = new Library;
+
+		provider_regular = new MockGlyphProvider(library, "DocumentContext");
+		provider_regular->SetWeight(eWeight_Normal);
+		provider_regular->SetStyle(eStyle_None);
+
+		provider_bold = new MockGlyphProvider(library, "DocumentContext");
+		provider_bold->SetWeight(eWeight_Bold);
+		provider_bold->SetStyle(eStyle_None);
+
+		provider_italic = new MockGlyphProvider(library, "DocumentContext");
+		provider_italic->SetWeight(eWeight_Normal);
+		provider_italic->SetStyle(eStyle_Italicized);
+
+		provider_bolditalic = new MockGlyphProvider(library, "DocumentContext");
+		provider_bolditalic->SetWeight(eWeight_Bold);
+		provider_bolditalic->SetStyle(eStyle_Italicized);
+
+		format = new TextFormat(library);
+		format->SetFamilyName("DocumentContext");
+		format->SetSize(12.0f);
+
+		parser = new TextParserMarkdown;
+
+		document = new TextLayoutDocument(library);
+		document->SetDefaultTextFormat(*format);
+		document->SetParser(parser);
+	}
+
+	void TearDown()
+	{
+		delete document;
+		delete format;
+		delete library;
+	}
+
+protected:
+
+	Library* library;
+	MockGlyphProvider* provider_regular;
+	MockGlyphProvider* provider_bold;
+	MockGlyphProvider* provider_italic;
+	MockGlyphProvider* provider_bolditalic;
+	TextFormat* format;
+	TextLayoutDocument* document;
+	TextParserMarkdown* parser;
+
+};
+
+TEST_F(TextLayoutDocumentContext, SetCharacter)
+{
+	document->SetText("Y");
+	document->Layout();
+
+	const std::vector<TextLayoutLine*>& lines = document->GetLines();
+	EXPECT_EQ(1, lines.size());
+
+	TextLayoutLine* line = lines[0];
+	EXPECT_EQ(1, line->GetChildrenCount());
+	EXPECT_VEC2_EQ(2.0f, 2.0f, line->GetElementGeometry().GetMinimum());
+	EXPECT_VEC2_EQ(10.0f, 10.0f, line->GetElementGeometry().GetMaximum());
+	EXPECT_VEC2_EQ(0.0f, -2.0f, line->GetLayoutGeometry().GetMinimum());
+	EXPECT_VEC2_EQ(12.0f, 16.0f, line->GetLayoutGeometry().GetMaximum());
+
+	TextLayoutCharacter* character = dynamic_cast<TextLayoutCharacter*>(line->GetChildAtIndex(0));
+	ASSERT_NE(nullptr, character);
+
+	EXPECT_EQ('Y', character->GetCodepoint());
+	EXPECT_VEC2_EQ(0.0f, 0.0f, character->GetPosition());
+	EXPECT_VEC2_EQ(0.0f, 0.0f, character->GetKerningAdjustment());
+	EXPECT_VEC2_EQ(2.0f, 2.0f, character->GetElementGeometry().GetMinimum());
+	EXPECT_VEC2_EQ(10.0f, 10.0f, character->GetElementGeometry().GetMaximum());
+	EXPECT_VEC2_EQ(0.0f, -2.0f, character->GetLayoutGeometry().GetMinimum());
+	EXPECT_VEC2_EQ(12.0f, 16.0f, character->GetLayoutGeometry().GetMaximum());
+}
 
 TEST(TextLayoutDocument, ParseText)
 {
