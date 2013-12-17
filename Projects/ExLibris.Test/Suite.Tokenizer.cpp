99b7c0988ba69b13c03036a911a3bb90371c44ed
Anne <knight666@gmail.com>
2013-12-17 15:18:24 +0100

    Suite.Tokenizer: Improved error results for tests.

--- Projects/ExLibris.Test/Suite.Tokenizer.cpp
+++ Projects/ExLibris.Test/Suite.Tokenizer.cpp
@@ -32,40 +32,95 @@ inline const char* TokenTypeToString(Token::Type a_Type)
 	}
 }
 
-::testing::AssertionResult ExpectTokenType(
+::testing::AssertionResult CompareToken(
 	const char* a_ExpressionLeft, const char* a_ExpressionRight,
-	Token::Type a_TypeLeft, Token::Type a_TypeRight
+	const Token& a_TokenLeft, const Token& a_TokenRight
 )
 {
-	if (a_TypeLeft == a_TypeRight)
+	if (
+		a_TokenLeft.type == a_TokenRight.type && 
+		a_TokenLeft.text == a_TokenRight.text &&
+		a_TokenLeft.column == a_TokenRight.column &&
+		a_TokenLeft.line == a_TokenRight.line
+	)
 	{
 		return ::testing::AssertionSuccess();
 	}
 	else
 	{
-		return ::testing::AssertionFailure()
-			<< "Type does not match:" << std::endl
-			<< "  Actual: " << TokenTypeToString(a_TypeRight) << std::endl
-			<< "Expected: " << TokenTypeToString(a_TypeLeft);
+		::testing::AssertionResult result = ::testing::AssertionFailure();
+
+		result << "Incorrect token." << std::endl;
+
+		if (a_TokenLeft.type != a_TokenRight.type)
+		{
+			result
+				<< std::endl
+				<< "[Type]" << std::endl
+				<< "   Actual:    " << TokenTypeToString(a_TokenRight.type) << std::endl
+				<< " Expected:    " << TokenTypeToString(a_TokenLeft.type) << std::endl;
+		}
+		else
+		{
+			result
+				<< std::endl
+				<< "[Type]        " << TokenTypeToString(a_TokenLeft.type) << std::endl;
+		}
+
+		if (a_TokenLeft.text != a_TokenRight.text)
+		{
+			result
+				<< std::endl
+				<< "[Text]" << std::endl
+				<< "   Actual:    \"" << a_TokenRight.text << "\"" << std::endl
+				<< " Expected:    \"" << a_TokenLeft.text << "\"" << std::endl;
+		}
+		else
+		{
+			result
+				<< std::endl
+				<< "[Text]        \"" << a_TokenLeft.text << "\"" << std::endl;
+		}
+
+		if (a_TokenLeft.column != a_TokenRight.column || a_TokenLeft.line != a_TokenRight.line)
+		{
+			result
+				<< std::endl
+				<< "[Position]" << std::endl
+				<< "   Actual:    line " << a_TokenRight.line << " column " << a_TokenRight.column << std::endl
+				<< " Expected:    line " << a_TokenLeft.line << " column " << a_TokenLeft.column << std::endl;
+		}
+		else
+		{
+			result
+				<< std::endl
+				<< "[Position]    line " << a_TokenLeft.line << " column " << a_TokenLeft.column << std::endl;
+		}
+
+		return result;
 	}
 }
 
 #define EXPECT_TOKEN(_type, _text, _column, _line) { \
 	EXPECT_TRUE(tk.ReadToken()); \
-	const Token& t = tk.GetCurrentToken(); \
-	EXPECT_PRED_FORMAT2(ExpectTokenType, _type, t.type); \
-	EXPECT_STREQ(_text, t.text.c_str()); \
-	EXPECT_EQ(_column, t.column); \
-	EXPECT_EQ(_line, t.line); \
+	const Token& a = tk.GetCurrentToken(); \
+	Token e; \
+	e.type = _type; \
+	e.text = _text; \
+	e.column = _column; \
+	e.line = _line; \
+	EXPECT_PRED_FORMAT2(CompareToken, e, a); \
 }
 
 #define EXPECT_END_TOKEN(_column, _line) { \
 	EXPECT_FALSE(tk.ReadToken()); \
-	const Token& t = tk.GetCurrentToken(); \
-	EXPECT_PRED_FORMAT2(ExpectTokenType, Token::eType_End, t.type); \
-	EXPECT_STREQ("", t.text.c_str()); \
-	EXPECT_EQ(_column, t.column); \
-	EXPECT_EQ(_line, t.line); \
+	const Token& a = tk.GetCurrentToken(); \
+	Token e; \
+	e.type = Token::eType_End; \
+	e.text = ""; \
+	e.column = _column; \
+	e.line = _line; \
+	EXPECT_PRED_FORMAT2(CompareToken, e, a); \
 }
 
 TEST(Tokenizer, Construct)
