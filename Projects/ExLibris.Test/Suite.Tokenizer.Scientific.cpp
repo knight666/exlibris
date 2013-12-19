932ce2005ebc4a9ea8306f686d0bfed515b0ae01
Anne <knight666@gmail.com>
2013-12-19 18:02:58 +0100

    Tokenizer: Improved parsing of scientific numbers. Suite.Tokenizer.Scientific: Added additional tests for starting from different types.

--- Projects/ExLibris.Test/Suite.Tokenizer.Scientific.cpp
+++ Projects/ExLibris.Test/Suite.Tokenizer.Scientific.cpp
@@ -16,3 +16,51 @@ TEST(TokenizerScientific, Read)
 	EXPECT_TOKEN(Token::eType_Scientific, "1.5e-6", 1, 1);
 	EXPECT_END_TOKEN(7, 1);
 }
+
+TEST(TokenizerScientific, ReadFromInteger)
+{
+	std::stringstream ss;
+	ss << "1e+12";
+
+	Tokenizer tk(&ss);
+
+	EXPECT_TOKEN(Token::eType_Scientific, "1e+12", 1, 1);
+	EXPECT_END_TOKEN(6, 1);
+}
+
+TEST(TokenizerScientific, ReadFromOctal)
+{
+	std::stringstream ss;
+	ss << "0e-0";
+
+	Tokenizer tk(&ss);
+
+	EXPECT_TOKEN(Token::eType_Scientific, "0e-0", 1, 1);
+	EXPECT_END_TOKEN(5, 1);
+}
+
+TEST(TokenizerScientific, ReadNumberNoPower)
+{
+	std::stringstream ss;
+	ss << "2.65e+";
+
+	Tokenizer tk(&ss);
+
+	EXPECT_TOKEN(Token::eType_Number, "2.65", 1, 1);
+	EXPECT_TOKEN(Token::eType_Text, "e", 5, 1);
+	EXPECT_TOKEN(Token::eType_Symbol, "+", 6, 1);
+	EXPECT_END_TOKEN(7, 1);
+}
+
+TEST(TokenizerScientific, ReadNumberNoNaturalLog)
+{
+	std::stringstream ss;
+	ss << "0.5e12";
+
+	Tokenizer tk(&ss);
+
+	EXPECT_TOKEN(Token::eType_Number, "0.5", 1, 1);
+	EXPECT_TOKEN(Token::eType_Text, "e", 4, 1);
+	EXPECT_TOKEN(Token::eType_Integer, "12", 5, 1);
+	EXPECT_END_TOKEN(7, 1);
+}
