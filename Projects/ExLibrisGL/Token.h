1a6b4d052c55f3162d98cffa7e66c0c949dd96b7
Anne <knight666@gmail.com>
2013-12-24 08:26:06 +0100

    Tokenizer: Added documentation. Token: Added documentation.

--- Projects/ExLibrisGL/Token.h
+++ Projects/ExLibrisGL/Token.h
@@ -26,27 +26,136 @@
 
 namespace ExLibris
 {
+	/*!
+		\brief Container for token information.
 
+		A token is output from the Tokenizer. It has a type, a value, a column and a row.
+		This information is used for parsing programming languages and other text-based input.
+
+		Tokens are not intended to be constructed without a Tokenizer, but can be copied.
+
+		Unprintable
+		==============
+
+		Unprintable or control characters affect the output, but are not rendered themselves. Examples include
+		the tab character `\t`, the newline character `\n` and the linefeed character `\r`.
+
+		Every character between ASCII value 0 and 31 is classified or reserved as a control character.
+
+		Identifier
+		==============
+
+		Identifier tokens consist of at least one alphanumeric character and cannot start with a digit. They are
+		allowed to start and end with one or more underscores, but not dashes.
+
+		Can be enabled or disabled using Tokenizer::eOption_Identifiers.
+
+		Text
+		==============
+
+		Text tokens contain everything not captured by other token types.
+
+		String
+		==============
+
+		String literals are special text tokens that start and end with a quotation symbol. They are not
+		allowed to span multiple lines. Quote symbols found after opening a string token are not considered
+		new string tokens.
+
+		Can be enabled or disabled using Tokenizer::eOption_Strings.
+
+		Whitespace
+		=============
+
+		Whitespace comes in the form of space and tab characters. Each space will advance the column position
+		by one, while a tab will advance the column position by a value determined by the tab width and current
+		column position.
+
+		Leading whitespace can be ignored using Tokenizer::eOption_IgnoreLeadingWhitespace.
+
+		New line
+		=============
+
+		New line tokens advance the line position by one. They can consist of `\r`, `\n` or `\r\n` depending
+		on the settings.
+
+		New line settings can be set using Tokenizer::SetNewLinePolicy.
+
+		Symbol
+		==============
+
+		Symbol tokens always hold a single character and consist of printable characters that are neither
+		text nor digits, for example `(` and `@`.
+
+		Integer
+		==============
+
+		A base-10 integer token consists of one or more digits between 0 and 9. An integer starting with a 0
+		can become an octal token, while an integer split using a dot character can become a number token.
+
+		Can be enabled or disabled using Tokenizer::eOption_NumbersOnly.
+
+		Octal
+		==============
+
+		A base-8 integer that starts with 0, consisting of digits between 0 and 7. A zero followed by an `x`
+		character is interpreted as a hexadecimal token.
+
+		Can be enabled or disabled using Tokenizer::eOption_NumbersOnly.
+
+		Hexadecimal
+		==============
+
+		A base-16 integer that starts with `0x`, consisting of digits between 0 and 9 and letters between A and F.
+
+		Can be enabled or disabled using Tokenizer::eOption_NumbersOnly.
+
+		Number
+		==============
+
+		An integer separated by a single dot (`.`) character. Numbers divided by an `e` or `E` character can
+		become a scientific token.
+
+		Scientific
+		==============
+
+		Shorthand for a number with a lot of zero's, either a very small or a very large value. Valid
+		scientific tokens have a positive or negative power value.
+
+		Example:
+
+			1e3 becomes 1000
+			2.4e-4 becomes 0.00024
+
+		Can be enabled or disabled using Tokenizer::eOption_NumbersOnly.
+
+		End
+		==============
+
+		Token denoting the end of (valid) input.
+	*/
 	struct Token
 	{
 
 	public:
 
+		//! Types available for tokens
+
 		enum Type
 		{
-			eType_Unprintable,
-			eType_Identifier,
-			eType_Text,
-			eType_String,
-			eType_Whitespace,
-			eType_NewLine,
-			eType_Symbol,
-			eType_Integer,
-			eType_Octal,
-			eType_Hexadecimal,
-			eType_Number,
-			eType_Scientific,
-			eType_End
+			eType_Unprintable,   /**< Unprintable or control characters */
+			eType_Identifier,    /**< Alphanumeric characters */
+			eType_Text,          /**< Default */
+			eType_String,        /**< Text in quotation marks */
+			eType_Whitespace,    /**< Space or tab characters */
+			eType_NewLine,       /**< New line */
+			eType_Symbol,        /**< Printable, neither text nor digit */
+			eType_Integer,       /**< Base 10 integer, digits 0-9 */
+			eType_Octal,         /**< Base 8 integer, digits 0-7 */
+			eType_Hexadecimal,   /**< Base 16 integer, digits 0-9 and letters A-F */
+			eType_Number,        /**< Integer split by a floating point */
+			eType_Scientific,    /**< Shorthand for long floating point numbers */
+			eType_End            /**< End of input */
 		};
 
 	public:
--- Projects/ExLibrisGL/Tokenizer.h
+++ Projects/ExLibrisGL/Tokenizer.h
@@ -29,34 +29,152 @@
 namespace ExLibris
 {
 
+	/*!
+		\brief Converts text input into tokens.
+
+		This class reads input from a stream and converts it to Token instances.
+		Tokens are used for parsing programming languages and other text-based input.
+
+		For example, let's say a JSON file is being parsed and the following line is encountered:
+
+			"fuel": 15.9,
+		
+		This input will generate five tokens:
+
+			""fuel"" of type "string" at line 1 and column 1
+			":" of type "symbol" at line 1 and column 6
+			" " of type "whitespace" at line 1 and column 7
+			"15.9" of type "number" at line 1 and column 8
+			"," of type "symbol" at line 1 and column 12
+			"" of type "end" at line 1 and column 13
+
+		The JSON parser then acts on these tokens, without having to validate them beforehand.
+
+		Tokenization is a greedy process: the tokenizer will consume as much input as possible for a given token.
+		For example, the input "item32" results in one identifier token, instead of a text token and an integer token.
+
+		Note that the tokenizer can only read forwards. If an invalid token is encountered,
+		the input is rolled back and treated as valid tokens of a different type.
+
+		Use-case
+		=======
+
+		The tokenizer is intended to be used as follows:
+
+			Tokenizer tk;
+			tk.EnableOptions(my_enabled_options);
+			tk.DisableOptions(my_disabled_options);
+			tk.SetInput(my_input);
+
+			while (tk.ReadToken())
+			{
+				Process(tk.GetCurrentToken());
+			}
+
+		Note that options can be changed during processing.
+	*/
 	class Tokenizer
 	{
 
 	public:
 
+		//! Options for the tokenization.
+
 		enum Option
 		{
-			eOption_Identifiers = (1 << 0)
+			eOption_Identifiers = (1 << 0) /**< Allow identifiers in the output. If disabled, text tokens are generated instead. */
 		};
 	
 	public:
 	
 		Tokenizer(std::basic_istream<char>* a_Stream);
 		~Tokenizer();
+		
+		/*!
+			\brief Check if an option is enabled
 
+			\param a_Option The option to check.
+			\return True when enabled, false when disabled.
+			\sa IsOptionDisabled, EnableOptions
+		*/
 		bool IsOptionEnabled(Option a_Option) const;
+
+		/*!
+			\brief Enable one or more options
+
+			\param a_Options The bitwise-or of one or more options.
+			\sa DisableOptions, IsOptionEnabled
+		*/
 		void EnableOptions(unsigned int a_Options);
 
+		/*!
+			\brief Check if an option is disabled
+
+			\param a_Option The option to check.
+			\return True when disabled, false when enabled.
+			\sa IsOptionEnabled, DisableOptions
+		*/
 		bool IsOptionDisabled(Option a_Option) const;
+
+		/*!
+			\brief Disable one or more options
+
+			\param a_Options The bitwise-or of one or more options.
+			\sa EnableOptinos, IsOptionDisabled
+		*/
 		void DisableOptions(unsigned int a_Options);
 
+		/*!
+			\brief Sets the width of a tab character
+
+			The tab width denotes the *maximum* width in spaces of a tab character. Calculating
+			the width of a tab character is an involved process, because it depends on the
+			position of the character on the line. 
+
+			An invalid tab width will set the tab width to 1.
+
+			\param a_Width A width in spaces.
+		*/
 		void SetTabWidth(size_t a_Width);
 
+		/*!
+			\brief Set the input used for generating tokens
+
+			The input stream is used for reading characters to convert into tokens. If the input
+			is invalid, only an end token is available. The input is only read forwards, never
+			backwards.
+
+			\param a_Stream Input stream used for reading characters.
+		*/
 		void SetInput(std::basic_istream<char>* a_Stream);
 
+		/*!
+			\brief Get the current token
+
+			The current token is the one read most recently by ReadToken. No other tokens are
+			available.
+
+			\return The current token.
+			\sa ReadToken
+		*/
 		const Token& GetCurrentToken() const;
 
+		/*!
+			\brief Check if the next token is available
+
+			\return True if another token is available, false if not
+		*/
 		bool IsNextTokenAvailable() const;
+
+		/*!
+			\brief Reads a token from the input
+
+			A valid token is read from the input. The last token read
+			is always an end token and will return false.
+
+			\return True on success, false on failure
+			\sa IsNextTokenAvailable, GetCurrentToken
+		*/
 		bool ReadToken();
 
 	private:
