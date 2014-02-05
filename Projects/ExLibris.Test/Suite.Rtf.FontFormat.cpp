2a85a5869684f6488e61d28c1d1fa6c70f9199f1
Anne <knight666@gmail.com>
2014-02-05 10:52:12 +0100

    RtfFontFormat: Class for keeping track of font formatting properties. Suite.Rtf.FontFormat: Tests for font format.

---
+++ Projects/ExLibris.Test/Suite.Rtf.FontFormat.cpp
@@ -0,0 +1,117 @@
+#include "ExLibris.Test.PCH.h"
+
+#include <RtfFontFormat.h>
+
+#include "Tools.Parseable.h"
+
+using namespace ExLibris;
+using namespace ExLibris::Rtf;
+
+TEST(RtfFontFormat, Construct)
+{
+	RtfWorld w;
+	FontFormat ff(w);
+
+	EXPECT_FALSE(ff.IsBold());
+	EXPECT_FALSE(ff.IsItalic());
+}
+
+TEST(RtfFontFormat, ParseBoldOn)
+{
+	RtfWorld w;
+	FontFormat ff(w);
+
+	RtfParserState s;
+
+	RtfToken t;
+	t.type = RtfToken::eParseType_Command;
+	t.value = "b";
+
+	EXPECT_PARSE_HANDLED(ff.Parse(s, t));
+
+	EXPECT_TRUE(ff.IsBold());
+}
+
+TEST(RtfFontFormat, ParseBoldOff)
+{
+	RtfWorld w;
+	FontFormat ff(w);
+
+	RtfParserState s;
+
+	RtfToken t;
+	t.type = RtfToken::eParseType_Command;
+	t.value = "b";
+	t.parameter = 0;
+
+	EXPECT_PARSE_HANDLED(ff.Parse(s, t));
+
+	EXPECT_FALSE(ff.IsBold());
+}
+
+TEST(RtfFontFormat, ParseBoldInvalidParameter)
+{
+	RtfWorld w;
+	FontFormat ff(w);
+
+	RtfParserState s;
+
+	RtfToken t;
+	t.type = RtfToken::eParseType_Command;
+	t.value = "b";
+	t.parameter = 12;
+
+	EXPECT_PARSE_HANDLED(ff.Parse(s, t));
+
+	EXPECT_TRUE(ff.IsBold());
+}
+
+TEST(RtfFontFormat, ParseItalicOn)
+{
+	RtfWorld w;
+	FontFormat ff(w);
+
+	RtfParserState s;
+
+	RtfToken t;
+	t.type = RtfToken::eParseType_Command;
+	t.value = "i";
+
+	EXPECT_PARSE_HANDLED(ff.Parse(s, t));
+
+	EXPECT_TRUE(ff.IsItalic());
+}
+
+TEST(RtfFontFormat, ParseItalicOff)
+{
+	RtfWorld w;
+	FontFormat ff(w);
+
+	RtfParserState s;
+
+	RtfToken t;
+	t.type = RtfToken::eParseType_Command;
+	t.value = "i";
+	t.parameter = 0;
+
+	EXPECT_PARSE_HANDLED(ff.Parse(s, t));
+
+	EXPECT_FALSE(ff.IsItalic());
+}
+
+TEST(RtfFontFormat, ParseItalicInvalidParameter)
+{
+	RtfWorld w;
+	FontFormat ff(w);
+
+	RtfParserState s;
+
+	RtfToken t;
+	t.type = RtfToken::eParseType_Command;
+	t.value = "i";
+	t.parameter = -9;
+
+	EXPECT_PARSE_HANDLED(ff.Parse(s, t));
+
+	EXPECT_TRUE(ff.IsItalic());
+}
